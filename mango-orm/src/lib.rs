3aa4712227c97baeaf511ec1450bfd7ae78b16c3
kebasyaty <kebasyaty@gmail.com>
2020-08-26 15:13:54 +0300

    Update

--- mango-orm/src/lib.rs
+++ mango-orm/src/lib.rs
@@ -29,7 +29,7 @@ mod tests {
     // Testing of Client
     // cargo test test_client -- --nocapture
     #[tokio::test]
-    async fn test_client() {
+    async fn test_client() -> Result<(), Box<dyn std::error::Error>> {
         let client_options = ClientOptions::builder()
             .hosts(vec![StreamAddress {
                 hostname: "localhost".into(),
@@ -37,10 +37,12 @@ mod tests {
             }])
             .build();
 
-        let client = Client::with_options(client_options).unwrap();
+        let client = Client::with_options(client_options)?;
 
-        for db_name in client.list_database_names(None, None).await.unwrap() {
+        for db_name in client.list_database_names(None, None).await? {
             println!("{}", db_name);
         }
+
+        Ok(())
     }
 }
