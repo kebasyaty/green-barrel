6f774248053d6b40894e5e020e7948be37f4600e
kebasyaty <kebasyaty@gmail.com>
2020-08-29 18:21:54 +0300

    Restructuring

--- mango-orm/src/widgets.rs
+++ mango-orm/src/widgets.rs
@@ -2,224 +2,230 @@
 //!
 //! Widgets for Forms.
 
-pub use widgets::*;
+pub use widgets::{relation_widgets::*, selection_widgets::*, standard_widgets::*};
 
 // WIDGETS =========================================================================================
 pub mod widgets {
 
     // Standard widgets ----------------------------------------------------------------------------
-    /// Enumeration for standard types
-    #[derive(Debug, Clone)]
-    pub enum StandardType {
-        CheckBox,
-        Color,
-        Date,
-        Email,
-        File,
-        Hidden,
-        Image,
-        Number,
-        Password,
-        Radio,
-        Range,
-        Tel,
-        Text,
-        Time,
-        Url,
-        TextArea,
-    }
-    impl Default for StandardType {
-        fn default() -> Self {
-            StandardType::Text
+    pub mod standard_widgets {
+        /// Enumeration for standard types
+        #[derive(Debug, Clone)]
+        pub enum StandardType {
+            CheckBox,
+            Color,
+            Date,
+            Email,
+            File,
+            Hidden,
+            Image,
+            Number,
+            Password,
+            Radio,
+            Range,
+            Tel,
+            Text,
+            Time,
+            Url,
+            TextArea,
         }
-    }
-    impl StandardType {
-        pub fn get_type(&self) -> String {
-            match self {
-                Self::CheckBox => "checkbox".to_string(),
-                Self::Color => "color".to_string(),
-                Self::Date => "date".to_string(),
-                Self::Email => "email".to_string(),
-                Self::File => "file".to_string(),
-                Self::Hidden => "hidden".to_string(),
-                Self::Image => "image".to_string(),
-                Self::Number => "number".to_string(),
-                Self::Password => "password".to_string(),
-                Self::Radio => "radio".to_string(),
-                Self::Range => "range".to_string(),
-                Self::Tel => "tel".to_string(),
-                Self::Text => "text".to_string(),
-                Self::Time => "time".to_string(),
-                Self::Url => "url".to_string(),
-                Self::TextArea => "textarea".to_string(),
+        impl Default for StandardType {
+            fn default() -> Self {
+                StandardType::Text
             }
         }
-    }
-
-    /// Default data types
-    #[derive(Debug, Clone)]
-    pub enum DefaultDataType {
-        Text(String),
-        I64(i64),
-        U64(u64),
-        F64(f64),
-    }
-    impl Default for DefaultDataType {
-        fn default() -> Self {
-            DefaultDataType::Text(String::new())
-        }
-    }
-    impl DefaultDataType {
-        pub fn get_data(&self) -> String {
-            match self {
-                Self::Text(data) => data.to_string(),
-                Self::I64(data) => data.to_string(),
-                Self::U64(data) => data.to_string(),
-                Self::F64(data) => data.to_string(),
+        impl StandardType {
+            pub fn get_type(&self) -> String {
+                match self {
+                    Self::CheckBox => "checkbox".to_string(),
+                    Self::Color => "color".to_string(),
+                    Self::Date => "date".to_string(),
+                    Self::Email => "email".to_string(),
+                    Self::File => "file".to_string(),
+                    Self::Hidden => "hidden".to_string(),
+                    Self::Image => "image".to_string(),
+                    Self::Number => "number".to_string(),
+                    Self::Password => "password".to_string(),
+                    Self::Radio => "radio".to_string(),
+                    Self::Range => "range".to_string(),
+                    Self::Tel => "tel".to_string(),
+                    Self::Text => "text".to_string(),
+                    Self::Time => "time".to_string(),
+                    Self::Url => "url".to_string(),
+                    Self::TextArea => "textarea".to_string(),
+                }
             }
         }
-    }
 
-    /// For standard widgets
-    /// Use for:
-    /// <input type="checkbox">
-    /// <input type="color">
-    /// <input type="date">
-    /// <input type="email">
-    /// <input type="file">
-    /// <input type="hidden">
-    /// <input type="image">
-    /// <input type="number">
-    /// <input type="password">
-    /// <input type="radio">
-    /// <input type="range">
-    /// <input type="tel">
-    /// <input type="text">
-    /// <input type="time">
-    /// <input type="url">
-    /// <textarea></textarea>
-    #[derive(Default, Debug)]
-    pub struct StandardWidget {
-        pub id: String, // "id-name" or auto
-        pub label: String,
-        pub input_type: StandardType,
-        pub default: DefaultDataType,
-        pub readonly: bool,
-        pub required: bool,
-        pub hint: String,
-        pub unique: bool,
-        pub hidden: bool,
-        pub other_attrs: String,   // "autofocus ..."
-        pub other_classes: String, // "class-name class-name ..."
-    }
+        /// Default data types
+        #[derive(Debug, Clone)]
+        pub enum DefaultDataType {
+            Text(String),
+            I64(i64),
+            U64(u64),
+            F64(f64),
+        }
+        impl Default for DefaultDataType {
+            fn default() -> Self {
+                DefaultDataType::Text(String::new())
+            }
+        }
+        impl DefaultDataType {
+            pub fn get_data(&self) -> String {
+                match self {
+                    Self::Text(data) => data.to_string(),
+                    Self::I64(data) => data.to_string(),
+                    Self::U64(data) => data.to_string(),
+                    Self::F64(data) => data.to_string(),
+                }
+            }
+        }
 
-    impl StandardWidget {
-        // Get attributes
-        pub fn attrs(&self) -> Self {
-            Self {
-                id: self.id.clone(),
-                label: self.label.clone(),
-                input_type: self.input_type.clone(),
-                default: self.default.clone(),
-                readonly: self.readonly.clone(),
-                required: self.required.clone(),
-                hint: self.hint.clone(),
-                unique: self.unique.clone(),
-                hidden: self.hidden.clone(),
-                other_attrs: self.other_attrs.clone(),
-                other_classes: self.other_classes.clone(),
+        /// For standard widgets
+        /// Use for:
+        /// <input type="checkbox">
+        /// <input type="color">
+        /// <input type="date">
+        /// <input type="email">
+        /// <input type="file">
+        /// <input type="hidden">
+        /// <input type="image">
+        /// <input type="number">
+        /// <input type="password">
+        /// <input type="radio">
+        /// <input type="range">
+        /// <input type="tel">
+        /// <input type="text">
+        /// <input type="time">
+        /// <input type="url">
+        /// <textarea></textarea>
+        #[derive(Default, Debug)]
+        pub struct StandardWidget {
+            pub id: String, // "id-name" or auto
+            pub label: String,
+            pub input_type: StandardType,
+            pub default: DefaultDataType,
+            pub readonly: bool,
+            pub required: bool,
+            pub hint: String,
+            pub unique: bool,
+            pub hidden: bool,
+            pub other_attrs: String,   // "autofocus ..."
+            pub other_classes: String, // "class-name class-name ..."
+        }
+
+        impl StandardWidget {
+            // Get attributes
+            pub fn attrs(&self) -> Self {
+                Self {
+                    id: self.id.clone(),
+                    label: self.label.clone(),
+                    input_type: self.input_type.clone(),
+                    default: self.default.clone(),
+                    readonly: self.readonly.clone(),
+                    required: self.required.clone(),
+                    hint: self.hint.clone(),
+                    unique: self.unique.clone(),
+                    hidden: self.hidden.clone(),
+                    other_attrs: self.other_attrs.clone(),
+                    other_classes: self.other_classes.clone(),
+                }
             }
         }
     }
     // Widget for choices items --------------------------------------------------------------------
-    /// Widget for choices items
-    /// Use for:
-    /// <select></select>
-    #[derive(Default, Debug)]
-    pub struct SelectionWidget {
-        pub id: String, // "id-name" or auto
-        pub label: String,
-        pub default: String,
-        pub disabled: bool,
-        pub multiple: bool,
-        pub required: bool,
-        pub hint: String,
-        pub other_attrs: String,   // "autofocus size='3'"
-        pub other_classes: String, // "class-name class-name ..."
-        pub select: Vec<(String, String)>,
-    }
+    pub mod selection_widgets {
+        /// Widget for choices items
+        /// Use for:
+        /// <select></select>
+        #[derive(Default, Debug)]
+        pub struct SelectionWidget {
+            pub id: String, // "id-name" or auto
+            pub label: String,
+            pub default: String,
+            pub disabled: bool,
+            pub multiple: bool,
+            pub required: bool,
+            pub hint: String,
+            pub other_attrs: String,   // "autofocus size='3'"
+            pub other_classes: String, // "class-name class-name ..."
+            pub select: Vec<(String, String)>,
+        }
 
-    impl SelectionWidget {
-        // Get attributes
-        pub fn attrs(&self) -> Self {
-            Self {
-                id: self.id.clone(),
-                label: self.label.clone(),
-                default: self.default.clone(),
-                disabled: self.disabled.clone(),
-                multiple: self.multiple.clone(),
-                required: self.required.clone(),
-                hint: self.hint.clone(),
-                other_attrs: self.other_attrs.clone(),
-                other_classes: self.other_classes.clone(),
-                select: self.select.clone(),
+        impl SelectionWidget {
+            // Get attributes
+            pub fn attrs(&self) -> Self {
+                Self {
+                    id: self.id.clone(),
+                    label: self.label.clone(),
+                    default: self.default.clone(),
+                    disabled: self.disabled.clone(),
+                    multiple: self.multiple.clone(),
+                    required: self.required.clone(),
+                    hint: self.hint.clone(),
+                    other_attrs: self.other_attrs.clone(),
+                    other_classes: self.other_classes.clone(),
+                    select: self.select.clone(),
+                }
             }
         }
     }
     // Widget  for relation fields -----------------------------------------------------------------
-    /// Enumeration of relationship types
-    #[derive(Debug, Clone)]
-    pub enum RelationType {
-        ForeignKey,
-        ManyToMany,
-        OneToOne,
-    }
-
-    impl Default for RelationType {
-        fn default() -> Self {
-            RelationType::ForeignKey
+    pub mod relation_widgets {
+        /// Enumeration of relationship types
+        #[derive(Debug, Clone)]
+        pub enum RelationType {
+            ForeignKey,
+            ManyToMany,
+            OneToOne,
         }
-    }
-    impl RelationType {
-        pub fn get_token(&self) -> String {
-            match self {
-                Self::ForeignKey => "m2o".to_string(),
-                Self::ManyToMany => "m2m".to_string(),
-                Self::OneToOne => "o2o".to_string(),
+
+        impl Default for RelationType {
+            fn default() -> Self {
+                RelationType::ForeignKey
             }
         }
-    }
+        impl RelationType {
+            pub fn get_token(&self) -> String {
+                match self {
+                    Self::ForeignKey => "m2o".to_string(),
+                    Self::ManyToMany => "m2m".to_string(),
+                    Self::OneToOne => "o2o".to_string(),
+                }
+            }
+        }
 
-    /// Widget for relation fields
-    /// Use for:
-    /// <select></select>
-    /// <select multiple></select> for ManyToMany type
-    #[derive(Default, Debug)]
-    pub struct RelationWidget {
-        pub id: String, // "id-name" or auto
-        pub label: String,
-        pub relation_type: RelationType, // Default = ForeignKey
-        pub readonly: bool,
-        pub required: bool,
-        pub hint: String,
-        pub hidden: bool,
-        pub other_attrs: String,   // "autofocus ..."
-        pub other_classes: String, // "class-name class-name ..."
-    }
+        /// Widget for relation fields
+        /// Use for:
+        /// <select></select>
+        /// <select multiple></select> for ManyToMany type
+        #[derive(Default, Debug)]
+        pub struct RelationWidget {
+            pub id: String, // "id-name" or auto
+            pub label: String,
+            pub relation_type: RelationType, // Default = ForeignKey
+            pub readonly: bool,
+            pub required: bool,
+            pub hint: String,
+            pub hidden: bool,
+            pub other_attrs: String,   // "autofocus ..."
+            pub other_classes: String, // "class-name class-name ..."
+        }
 
-    impl RelationWidget {
-        // Get attributes
-        pub fn attrs(&self) -> Self {
-            Self {
-                id: self.id.clone(),
-                label: self.label.clone(),
-                relation_type: self.relation_type.clone(),
-                readonly: self.readonly.clone(),
-                required: self.required.clone(),
-                hint: self.hint.clone(),
-                hidden: self.hidden.clone(),
-                other_attrs: self.other_attrs.clone(),
-                other_classes: self.other_classes.clone(),
+        impl RelationWidget {
+            // Get attributes
+            pub fn attrs(&self) -> Self {
+                Self {
+                    id: self.id.clone(),
+                    label: self.label.clone(),
+                    relation_type: self.relation_type.clone(),
+                    readonly: self.readonly.clone(),
+                    required: self.required.clone(),
+                    hint: self.hint.clone(),
+                    hidden: self.hidden.clone(),
+                    other_attrs: self.other_attrs.clone(),
+                    other_classes: self.other_classes.clone(),
+                }
             }
         }
     }
