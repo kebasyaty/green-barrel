03658649689a2e18763182235f474fb1700e4db2
kebasyaty <kebasyaty@gmail.com>
2020-09-01 12:21:26 +0300

    Update

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -1,12 +1,12 @@
 use mango_orm::widgets::{DataType, Widget};
 
 fn main() {
-    let field = Widget {
+    let widget = Widget {
         label: "Planets",
         value: DataType::F64(10.3),
         required: true,
         ..Default::default()
     };
 
-    println!("{:#?}", field);
+    println!("{:#?}", widget);
 }
