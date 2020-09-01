56356fdc088b9f2f780cd4bb3ccd4e58bf9660fc
kebasyaty <kebasyaty@gmail.com>
2020-09-01 17:26:52 +0300

    Update

--- mango-orm/src/models.rs
+++ mango-orm/src/models.rs
@@ -6,55 +6,130 @@
 /// Field Types for Models
 #[derive(Debug, Clone)]
 pub enum ModelsFieldType {
-    CheckBox,
-    Color,
-    Date,
-    Email,
-    File,
-    Hidden,
-    Image,
-    Number,
-    Password,
-    Radio,
-    Range,
-    Tel,
-    Text,
-    Time,
-    Url,
-    TextArea,
-    Select,
-    ForeignKey,
-    ManyToMany,
-    OneToOne,
+    CheckBox(Option<bool>),
+    Color(Option<String>),
+    Date(Option<String>),
+    Email(Option<String>),
+    File(Option<String>),
+    Image(Option<String>),
+    NumberI64(Option<i64>),
+    NumberU64(Option<u64>),
+    NumberF64(Option<f64>),
+    Password(Option<String>),
+    Radio(Option<bool>),
+    Tel(Option<String>),
+    Text(Option<String>),
+    Time(Option<String>),
+    Url(Option<String>),
+    TextArea(Option<String>),
+    SelectText(Option<String>),
+    SelectI64(Option<i64>),
+    SelectU64(Option<u64>),
+    SelectF64(Option<f64>),
+    ForeignKey(Option<String>),
+    ManyToMany(Option<String>),
+    OneToOne(Option<String>),
 }
 impl Default for ModelsFieldType {
     fn default() -> Self {
-        ModelsFieldType::Text
+        ModelsFieldType::Text(Some(String::new()))
     }
 }
 impl ModelsFieldType {
     pub fn get_type(&self) -> String {
         match self {
-            Self::CheckBox => "checkbox".to_string(),
-            Self::Color => "color".to_string(),
-            Self::Date => "date".to_string(),
-            Self::Email => "email".to_string(),
-            Self::File => "file".to_string(),
-            Self::Hidden => "hidden".to_string(),
-            Self::Image => "image".to_string(),
-            Self::Number => "number".to_string(),
-            Self::Password => "password".to_string(),
-            Self::Radio => "radio".to_string(),
-            Self::Range => "range".to_string(),
-            Self::Tel => "tel".to_string(),
-            Self::Text => "text".to_string(),
-            Self::Time => "time".to_string(),
-            Self::Url => "url".to_string(),
-            Self::TextArea => "textarea".to_string(),
-            Self::Select => "select".to_string(),
-            Self::ForeignKey => "m2o".to_string(),
-            Self::ManyToMany => "m2m".to_string(),
-            Self::OneToOne => "o2o".to_string(),
+            Self::CheckBox(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Color(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Date(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Email(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::File(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Image(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::NumberI64(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::NumberU64(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::NumberF64(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Password(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Radio(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Tel(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Text(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Time(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::Url(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::TextArea(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::SelectText(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::SelectI64(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::SelectU64(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::SelectF64(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::ForeignKey(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::ManyToMany(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
+            Self::OneToOne(data) => match data {
+                Some(data) => data.to_string(),
+                None => "Option::None".to_string(),
+            },
         }
     }
 }
