07bb9066079ec76669f162fb26713e81c6ca0a38
kebasyaty <kebasyaty@gmail.com>
2020-09-02 08:25:57 +0300

    Update

--- mango-orm/src/models.rs
+++ mango-orm/src/models.rs
@@ -4,132 +4,190 @@
 
 // MODELS ==========================================================================================
 /// Field Types for Models
+/// CheckBox
 #[derive(Debug, Clone)]
-pub enum ModelsFieldType {
-    CheckBox(Option<bool>),
-    Color(Option<String>),
-    Date(Option<String>),
-    Email(Option<String>),
-    File(Option<String>),
-    Image(Option<String>),
-    NumberI64(Option<i64>),
-    NumberU64(Option<u64>),
-    NumberF64(Option<f64>),
-    Password(Option<String>),
-    Radio(Option<bool>),
-    Tel(Option<String>),
-    Text(Option<String>),
-    Time(Option<String>),
-    Url(Option<String>),
-    TextArea(Option<String>),
-    SelectText(Option<String>),
-    SelectI64(Option<i64>),
-    SelectU64(Option<u64>),
-    SelectF64(Option<f64>),
-    ForeignKey(Option<String>),
-    ManyToMany(Option<String>),
-    OneToOne(Option<String>),
+pub enum CheckBox {
+    Data(Option<bool>),
 }
-impl Default for ModelsFieldType {
+impl Default for CheckBox {
     fn default() -> Self {
-        ModelsFieldType::Text(Option::None)
+        CheckBox::Data(Option::None)
     }
 }
-impl ModelsFieldType {
-    pub fn get_data(&self) -> String {
+impl CheckBox {
+    pub fn get_data(&self) -> Option<bool> {
         match self {
-            Self::CheckBox(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Color(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Date(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Email(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::File(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Image(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::NumberI64(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::NumberU64(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::NumberF64(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Password(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Radio(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Tel(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Text(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Time(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::Url(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::TextArea(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::SelectText(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::SelectI64(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::SelectU64(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::SelectF64(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::ForeignKey(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::ManyToMany(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
-            Self::OneToOne(data) => match data {
-                Some(data) => data.to_string(),
-                None => "Option::None".to_string(),
-            },
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// Color
+#[derive(Debug, Clone)]
+pub enum Color {
+    Data(Option<&'static str>),
+}
+impl Default for Color {
+    fn default() -> Self {
+        Color::Data(Option::None)
+    }
+}
+impl Color {
+    pub fn get_data(&self) -> Option<&'static str> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// Date
+#[derive(Debug, Clone)]
+pub enum Date {
+    Data(Option<&'static str>),
+}
+impl Default for Date {
+    fn default() -> Self {
+        Date::Data(Option::None)
+    }
+}
+impl Date {
+    pub fn get_data(&self) -> Option<&'static str> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// Email
+#[derive(Debug, Clone)]
+pub enum Email {
+    Data(Option<&'static str>),
+}
+impl Default for Email {
+    fn default() -> Self {
+        Email::Data(Option::None)
+    }
+}
+impl Email {
+    pub fn get_data(&self) -> Option<&'static str> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// File
+#[derive(Debug, Clone)]
+pub enum File {
+    Data(Option<&'static str>),
+}
+impl Default for File {
+    fn default() -> Self {
+        File::Data(Option::None)
+    }
+}
+impl File {
+    pub fn get_data(&self) -> Option<&'static str> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// Image
+#[derive(Debug, Clone)]
+pub enum Image {
+    Data(Option<&'static str>),
+}
+impl Default for Image {
+    fn default() -> Self {
+        Image::Data(Option::None)
+    }
+}
+impl Image {
+    pub fn get_data(&self) -> Option<&'static str> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// NumberI64
+#[derive(Debug, Clone)]
+pub enum NumberI64 {
+    Data(Option<i64>),
+}
+impl Default for NumberI64 {
+    fn default() -> Self {
+        NumberI64::Data(Option::None)
+    }
+}
+impl NumberI64 {
+    pub fn get_data(&self) -> Option<i64> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// NumberU64
+#[derive(Debug, Clone)]
+pub enum NumberU64 {
+    Data(Option<i64>),
+}
+impl Default for NumberU64 {
+    fn default() -> Self {
+        NumberU64::Data(Option::None)
+    }
+}
+impl NumberU64 {
+    pub fn get_data(&self) -> Option<i64> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// NumberF64
+#[derive(Debug, Clone)]
+pub enum NumberF64 {
+    Data(Option<f64>),
+}
+impl Default for NumberF64 {
+    fn default() -> Self {
+        NumberF64::Data(Option::None)
+    }
+}
+impl NumberF64 {
+    pub fn get_data(&self) -> Option<f64> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// Password
+#[derive(Debug, Clone)]
+pub enum Password {
+    Data(Option<&'static str>),
+}
+impl Default for Password {
+    fn default() -> Self {
+        Password::Data(Option::None)
+    }
+}
+impl Password {
+    pub fn get_data(&self) -> Option<&'static str> {
+        match self {
+            Self::Data(data) => *data,
+        }
+    }
+}
+/// Radio
+#[derive(Debug, Clone)]
+pub enum Radio {
+    Data(Option<bool>),
+}
+impl Default for Radio {
+    fn default() -> Self {
+        Radio::Data(Option::None)
+    }
+}
+impl Radio {
+    pub fn get_data(&self) -> Option<bool> {
+        match self {
+            Self::Data(data) => *data,
         }
     }
 }
