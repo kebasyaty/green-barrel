77042af012e8242f6796cbab255dc5d3d5bd14ca
kebasyaty <kebasyaty@gmail.com>
2020-09-04 17:08:31 +0300

    Update

--- stamping/src/services/primal/mango_models.rs
+++ stamping/src/services/primal/mango_models.rs
@@ -22,26 +22,26 @@ impl Form for User {
         raw_attrs.insert(
             "username",
             Widget {
-                label: "Your Name".to_string(),
+                label: "Your Name:".to_string(),
                 field_type: FieldType::InputText,
                 value: DataType::Text("Rust".to_string()),
                 maxlength: 40,
                 required: true,
                 hint: "Please enter your real name.".to_string(),
-                other_attrs: format!("placeholder=\"{}\"", "Your Name"),
+                other_attrs: format!("placeholder={}", "Your Name"),
                 ..Default::default()
             },
         );
         raw_attrs.insert(
             "email",
             Widget {
-                label: "Your Email".to_string(),
+                label: "Your Email:".to_string(),
                 field_type: FieldType::InputEmail,
                 maxlength: 78,
                 required: true,
                 hint: "Enter your work email.".to_string(),
                 unique: true,
-                other_attrs: format!("placeholder=\"{}\"", "Your Email"),
+                other_attrs: format!("placeholder={}", "Your Email"),
                 ..Default::default()
             },
         );
