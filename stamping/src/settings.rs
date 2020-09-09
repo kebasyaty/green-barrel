4878145e94f428d30d310ba5c870fc2b13b43536
kebasyaty <kebasyaty@gmail.com>
2020-09-09 17:05:31 +0300

    Update

--- stamping/src/settings.rs
+++ stamping/src/settings.rs
@@ -8,14 +8,22 @@ pub use default_settings::*;
 pub mod default_settings {
     pub static DEBUG: bool = true;
     pub static PROJECT_NAME: &str = "Example";
+    // Http Server
     pub static LOCAL_DOMAIN: &str = "127.0.0.1";
     pub static PORT: u16 = 8088;
+    // Site
     pub static SITE_DOMAIN: &str = "www.site-name.net";
+    // Database
+    pub static DB_HOSTNAME: &str = "localhost";
+    pub static DB_PORT: u16 = 27017;
     // 2.016 mb
     pub static MAX_UPLOAD_SIZE: usize = (2.016 * 1024.0 * 1024.0) as usize;
+    // Security
     // http://www.miniwebtool.com/django-secret-key-generator/
     pub static SECRET_KEY: &str = "hf@$%#-ftw(ia4jualowaqlejtm17h*98pzqk18bd65um5_xnx";
+    // Session
     pub static SESSION_KEY: &[u8] = SECRET_KEY.as_bytes();
+    // Files
     pub static MEDIA_URL: &str = "/media/";
     pub static MEDIA_ROOT: &str = "./media/";
     pub static STATIC_URL: &str = "/static/";
