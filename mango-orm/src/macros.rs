68f9f923067e2b4678e4782f9808be753be46419
kebasyaty <kebasyaty@gmail.com>
2020-09-12 08:49:03 +0300

    Update

---
+++ mango-orm/src/macros.rs
@@ -0,0 +1,28 @@
+//! # Macros
+//!
+//!  Custom macros.
+
+/// Macro for converting Structure to Model
+#[macro_export]
+macro_rules! create_model {
+    (struct $name:ident { $($fname:ident : $ftype:ty),* }) => {
+        use serde::{Deserialize, Serialize};
+
+        #[derive(Serialize, Deserialize, Debug, Default)]
+        pub struct $name {
+            pub $($fname : $ftype),*
+        }
+
+        impl $name {
+            fn struct_name() -> &'static str {
+                static NAME: &'static str  = stringify!($name);
+                NAME
+            }
+
+            fn field_names() -> &'static [&'static str] {
+                static NAMES: &'static [&'static str] = &[$(stringify!($fname)),*];
+                NAMES
+            }
+        }
+    }
+}
