15ca4ff1d51b9fd9e02b957aaa47582db7696b01
kebasyaty <kebasyaty@gmail.com>
2020-09-17 13:39:52 +0300

    Update

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -5,6 +5,7 @@ mod mango_models;
 
 async fn mango_migration() {
     // KEYWORD - It is recommended not to change within the boundaries of one project
+    // (Valid characters: _|a-z|A-Z|0-9 ; Size: 8-16.)
     static KEYWORD: &'static str = "7rzg_cfqQB3B7q7T";
     let client: Client = Client::with_uri_str("mongodb://localhost:27017")
         .await
