3f9198301c9192b75de621ee75afc0cc74fd1227
kebasyaty <kebasyaty@gmail.com>
2020-09-17 18:58:38 +0300

    Update

--- mango-orm/src/macros.rs
+++ mango-orm/src/macros.rs
@@ -399,7 +399,7 @@ macro_rules! create_model {
 
                 // Check the field changes in the Model and (if required) update the documents in the appropriate Collection
                 for field in FIELD_NAMES {
-                    //
+                    println!("{}", field);
                 }
             }
         }
