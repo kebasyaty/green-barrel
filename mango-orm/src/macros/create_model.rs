a06d387b112e96c8b266cab320f22690e457f019
kebasyaty <kebasyaty@gmail.com>
2020-09-28 10:38:47 +0300

    Update

--- mango-orm/src/macros/create_model.rs
+++ mango-orm/src/macros/create_model.rs
@@ -91,15 +91,15 @@ macro_rules! create_model {
             pub async fn save(&self, client: &Client) -> Result<String, Box<dyn Error>> {
                 let meta: Meta = Self::meta()?;
                 let doc: Document = to_document(self).unwrap_or_else(|err| {
-                    panic!("{:?}", err);
+                    panic!("{:?}", err)
                 });
                 let coll: Collection = client.database(&meta.database).collection(&meta.collection);
                 let result = coll.insert_one(doc, None).await.unwrap_or_else(|err| {
-                    panic!("{:?}", err);
+                    panic!("{:?}", err)
                 });
                 let id: Option<&ObjectId> = result.inserted_id.as_object_id();
                 if id.is_none() {
-                    panic!("Database Query API -> Method `save()` did not return `ObjectId`.");
+                    panic!("Database Query API -> Method `save()` did not return `ObjectId`.")
                 }
                 Ok(id.unwrap().to_hex())
             }
