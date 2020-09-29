2d57581e6dfd848c3be67ac9d6829984c4a8d6d1
kebasyaty <kebasyaty@gmail.com>
2020-09-29 13:14:50 +0300

    Update

--- mango-orm/src/macros/model.rs
+++ mango-orm/src/macros/model.rs
@@ -132,11 +132,8 @@ macro_rules! model {
             pub async fn migrat<'a>(keyword: &'a str, client: &Client) {
                 static MODEL_NAME: &'static str = stringify!($sname);
                 static FIELD_NAMES: &'static [&'static str] = &[$(stringify!($fname)),*];
-                // List field names without `id` field
-                let field_names_no_id: Vec<&'static str> = FIELD_NAMES.iter()
-                    .map(|field| field.clone()).filter(|field| field != &"id").collect();
                 // Checking for the presence of fields
-                if field_names_no_id.len() == 0 {
+                if FIELD_NAMES.len() == 0 {
                     panic!("The model structure has no fields.");
                 }
                 // Create a map with field types
@@ -161,7 +158,7 @@ macro_rules! model {
                 // Looping over fields and attributes
                 for (field, widget) in map_widgets {
                     // Checking for the correct field name
-                    if !FIELD_NAMES.contains(&field) {
+                    if !FIELD_NAMES.contains(&field) && field != "id" {
                         panic!(
                             "Service: `{}` -> Model: `{}` -> widgets() : `{}` - Incorrect field name.",
                             $service, MODEL_NAME, field
@@ -558,11 +555,11 @@ macro_rules! model {
                     };
                     // Check if the set of fields in the collection of the current Model needs to be updated
                     let mut run_documents_modification: bool = false;
-                    if field_names_no_id.len() != mango_orm_fnames.len() {
+                    if FIELD_NAMES.len() != mango_orm_fnames.len() {
                         run_documents_modification = true;
                     } else {
-                        for item in field_names_no_id {
-                            if mango_orm_fnames.iter().any(|item2| item2 != &item) {
+                        for item in FIELD_NAMES {
+                            if mango_orm_fnames.iter().any(|item2| item2 != item) {
                                 run_documents_modification = true;
                                 break;
                             }
@@ -582,9 +579,6 @@ macro_rules! model {
                             let mut tmp_doc = doc! {};
                             // Loop over all fields of the model
                             for field in FIELD_NAMES {
-                                if field == &"id" {
-                                    continue;
-                                }
                                 // If the field exists, get its value
                                 if curr_doc.contains_key(field) {
                                     for item in curr_doc.iter() {
@@ -640,8 +634,7 @@ macro_rules! model {
                     let doc = doc!{
                         "database": &meta.database,
                         "collection": &meta.collection,
-                        "fields": FIELD_NAMES.iter().map(|item| item.to_string())
-                            .filter(|item| item != "id").collect::<Vec<String>>(),
+                        "fields": FIELD_NAMES,
                         "status": true
                     };
                     // Check if there is model state in the database
