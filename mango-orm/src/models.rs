18555eab5052e5d67d5abf88810384b0747dbaba
kebasyaty <kebasyaty@gmail.com>
2020-10-01 09:19:37 +0300

    Update

--- mango-orm/src/models.rs
+++ mango-orm/src/models.rs
@@ -7,14 +7,14 @@ use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-pub struct Cache {
+pub struct FormCache {
     pub form_map_attrs: HashMap<String, Transport>,
     pub form_json_attrs: String,
     pub form_html: String,
 }
 
 lazy_static! {
-    static ref CACHE: Mutex<HashMap<&'static str, HashMap<&'static str, HashMap<&'static str, Cache>>>> = {
+    static ref FORM_CACHE: Mutex<HashMap<&'static str, FormCache>> = {
         let mut _map = HashMap::new();
         Mutex::new(_map)
     };
