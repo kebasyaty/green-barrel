3c218db361be509780d2aab52424d3332878614f
kebasyaty <kebasyaty@gmail.com>
2020-10-07 13:49:25 +0300

    Update

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -42,6 +42,6 @@ async fn main() {
     let client: Client = Client::with_uri_str("mongodb://localhost:27017")
         .await
         .unwrap();
-    println!("{}", user.save(&client).await.unwrap().to_hash().unwrap());
-    println!("{}", user.save(&client).await.unwrap().to_hash().unwrap());
+    println!("{:?}", user.save(&client).await.unwrap().to_map().unwrap());
+    println!("{:?}", user.save(&client).await.unwrap().to_map().unwrap());
 }
