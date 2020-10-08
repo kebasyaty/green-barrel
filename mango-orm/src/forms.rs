34ebafd7581973ff73bb7e0e3f8b0a55cf241e4a
kebasyaty <kebasyaty@gmail.com>
2020-10-08 15:27:34 +0300

    Update

--- mango-orm/src/forms.rs
+++ mango-orm/src/forms.rs
@@ -29,14 +29,14 @@ pub trait Form {
         if build_controls {
             for (_, trans) in attrs {
                 let id_field = format!("{}--{}", model_name, trans.id);
-                let label = format!(
-                    "<p>{}",
-                    if trans.field_type != "hidden" {
-                        format!("<label for=\"{}\">{}:</label><br>", id_field, trans.label)
-                    } else {
-                        String::new()
-                    }
-                );
+                let label = if trans.field_type != "hidden" {
+                    format!(
+                        "<p><label for=\"{}\">{}:</label><br>",
+                        id_field, trans.label
+                    )
+                } else {
+                    String::new()
+                };
                 match trans.field_type.as_str() {
                     "text" | "url" | "tel" | "password" | "email" | "color" => {
                         controls = format!(
@@ -266,7 +266,7 @@ pub trait Form {
                     }
                     "hidden" => {
                         controls = format!(
-                            "{}{}<input id=\"{}\" type=\"{}\" name=\"{}\" value=\"{}\"{}{}{}></p>",
+                            "{}{}<input id=\"{}\" type=\"{}\" name=\"{}\" value=\"{}\"{}{}{}>",
                             controls,
                             label,
                             id_field,
