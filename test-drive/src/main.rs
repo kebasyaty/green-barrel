21e3d10325336471cac168f2816065d12484a017
kebasyaty <kebasyaty@gmail.com>
2020-10-13 18:09:26 +0300

    Update

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -36,7 +36,7 @@ async fn main() {
 
     let mut user = mango_models::User {
         username: "Rust".to_string(),
-        email: "test11@test.test".to_string(),
+        email: "test12@test.test".to_string(),
         password: "12345678".to_string(),
         password_confirm: "12345678".to_string(),
         ..Default::default()
