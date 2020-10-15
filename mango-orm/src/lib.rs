d7966f6ab5739d22e2a83a36bba8f4b42484b71b
kebasyaty <kebasyaty@gmail.com>
2020-10-15 12:14:45 +0300

    Update

--- mango-orm/src/lib.rs
+++ mango-orm/src/lib.rs
@@ -104,8 +104,8 @@ mod tests {
         assert!(!re.is_match("0000-01-01T24:00:00"));
         assert!(!re.is_match("0000-01-01T00:60:00"));
         assert!(!re.is_match("0000-01-01T00:00:60"));
-        assert!(re.is_match("0000-12-31T00:00:00"));
         assert!(re.is_match("0000-01-01T00:00:00"));
+        assert!(re.is_match("9999-12-31T00:00:00"));
         assert!(re.is_match("2020-10-15T11:17:49"));
     }
 }
