a0f86365abb74738eafa502952cc9ebf99d131c4
kebasyaty <kebasyaty@gmail.com>
2020-10-15 11:41:35 +0300

    Update

--- mango-orm/src/widgets.rs
+++ mango-orm/src/widgets.rs
@@ -22,7 +22,7 @@ pub enum FieldType {
     InputRadioF64(f64),
     InputColor(String),
     InputDate(String),
-    InputDateTime(String), // Example: "000-00-00T00:00:00"
+    InputDateTime(String), // Example: "0000-00-00T00:00:00"
     InputTimeStamp(u32),
     InputEmail(String),
     InputFile,
