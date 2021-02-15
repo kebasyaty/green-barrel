07b74d1f01dcd17ee4ef2b52a55c012c3cd2b65a
kebasyaty <kebasyaty@gmail.com>
2021-02-15 08:30:24 +0200

    Update settings.rs

--- test-drive/src/settings.rs
+++ test-drive/src/settings.rs
@@ -15,8 +15,8 @@ pub const PROJECT_NAME: &str = "project_name";
 // Example: "7rzgacfqQB3B7q7T"
 pub const UNIQUE_PROJECT_KEY: &str = "7rzgacfqQB3B7q7T";
 
-// For a test service/sub-application.
-pub mod service_name {
+// Default settings.
+pub mod default {
     // Valid characters: _ a-z A-Z 0-9
     // Max size: 31
     // First character: a-z A-Z
