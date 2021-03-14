7606cfd7359d1bd545aff94eefc357a8eaf51df1
kebasyaty <kebasyaty@gmail.com>
2021-03-14 17:03:10 +0200

    Update `test-drive` state

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -157,8 +157,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Test Model.
     let mut user = mango_models::UserProfile {
         username: Some("Rust".to_string()),
-        email: Some("test_15_@test.test".to_string()),
-        confirm_email: Some("test_15_@test.test".to_string()),
+        email: Some("test_16_@test.test".to_string()),
+        confirm_email: Some("test_16_@test.test".to_string()),
         password: Some("12345678".to_string()),
         confirm_password: Some("12345678".to_string()),
         date: Some("2020-12-19".to_string()),
@@ -172,7 +172,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ..Default::default() // or initialize the `hash` field - { hash: Some(String::new()) }
     };
 
-    println!("{}", user.json_for_admin()?);
+    //println!("{}", user.json_for_admin()?);
 
     // Create doc.
     let result = user.save(None, None, None)?;
