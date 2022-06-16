d12c75186f3727901e1d11e31b262c2f0682c67d
kebasyaty <kebasyaty@gmail.com>
2022-06-16 11:27:13 +0300

    Update test-drive

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -181,8 +181,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Test Model.
     let mut user = mango_models::UserProfile {
-        username: Some("user_29".to_string()),
-        email: Some("user_29_@noreply.net".to_string()),
+        username: Some("user_30".to_string()),
+        email: Some("user_30_@noreply.net".to_string()),
         password: Some("12345678".to_string()),
         confirm_password: Some("12345678".to_string()),
         is_staff: Some(false),
