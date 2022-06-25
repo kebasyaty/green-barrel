9e9feb1919b2389fc6409041ffc6077fd9b9b580
kebasyaty <kebasyaty@gmail.com>
2022-06-25 11:29:12 +0300

    Import optimized.

--- mango-orm/src/test_tool.rs
+++ mango-orm/src/test_tool.rs
@@ -1,22 +1,23 @@
 //! Auxiliary tools for testing models.
 
-use crate::store::MONGODB_CLIENT_STORE;
+use crate::{models::Meta, store::MONGODB_CLIENT_STORE};
+use mongodb::sync::Client;
+use std::error::Error;
 
 /// Remove test databases
 /// Hint: See the tests in the `test-drive` section for an example.
 pub fn del_test_db(
     project_name: &str,
     unique_project_key: &str,
-    models: &Vec<crate::models::Meta>,
-) -> Result<(), Box<dyn std::error::Error>> {
+    models: &Vec<Meta>,
+) -> Result<(), Box<dyn Error>> {
     // Name of the technical database for testing
     let db_mango_tech: String = format!("mango_tech__{}__{}", project_name, unique_project_key);
     //
     let client_store = MONGODB_CLIENT_STORE.read()?;
     // Removing databases
     for meta in models {
-        let client: &mongodb::sync::Client =
-            client_store.get(meta.db_client_name.as_str()).unwrap();
+        let client: &Client = client_store.get(meta.db_client_name.as_str()).unwrap();
         let database_names: Vec<String> = client.list_database_names(None, None)?;
         //
         if database_names.contains(&db_mango_tech) {
