b1e920a0d49b8cf32e68c04beb4065e7dba1ee7b
kebasyaty <kebasyaty@gmail.com>
2022-06-27 10:58:31 +0300

    fix to_prepared_doc method

--- mango-orm/src/models/output_data.rs
+++ mango-orm/src/models/output_data.rs
@@ -162,7 +162,10 @@ pub trait Converters {
                         Bson::Null
                     },
                 );
-            } else if widget_type == "inputDateTime" {
+            } else if widget_type == "inputDateTime"
+                || field_name == "created_at"
+                || field_name == "updated_at"
+            {
                 let bson_val = doc.get(field_name).unwrap();
                 prepared_doc.insert(
                     field_name,
