bf808320143ae74cd3580e00a8db60b0b137d52b
kebasyaty <kebasyaty@gmail.com>
2022-08-10 09:14:03 +0300

    Fix InputColor

--- green-barrel/src/widgets/input_color.rs
+++ green-barrel/src/widgets/input_color.rs
@@ -4,41 +4,41 @@ use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-pub struct InputColor<'a> {
-    pub id: &'a str, // The value is determined automatically. Format: "model-name--field-name".
-    pub label: &'a str, // Web form field name.
-    pub widget: &'a str, // Widget name.
-    pub input_type: &'a str, // The value is determined automatically.
-    pub name: &'a str, // The value is determined automatically.
+pub struct InputColor {
+    pub id: String, // The value is determined automatically. Format: "model-name--field-name".
+    pub label: String, // Web form field name.
+    pub widget: String, // Widget name.
+    pub input_type: String, // The value is determined automatically.
+    pub name: String, // The value is determined automatically.
     pub value: Option<String>, // Default value.
-    pub placeholder: &'a str, // Displays prompt text.
-    pub pattern: &'a str, // Validating a field using a client-side regex (Only for text, search, tel, url, email, and password controls).
+    pub placeholder: String, // Displays prompt text.
+    pub pattern: String, // Validating a field using a client-side regex (Only for text, search, tel, url, email, and password controls).
     pub minlength: usize, // The minimum number of characters allowed in the text.
     pub maxlength: usize, // The maximum number of characters allowed in the text.
-    pub required: bool,   // Mandatory field.
-    pub unique: bool,     // The unique value of a field in a collection.
-    pub disabled: bool,   // Blocks access and modification of the element.
-    pub readonly: bool,   // Specifies that the field cannot be modified by the user.
-    pub is_hide: bool,    // Hide field from user.
-    pub other_attrs: &'a str, // Example: r# "autofocus tabindex="some number" size="some number""#.
-    pub css_classes: &'a str, // Example: "class-name-1 class-name-2".
-    pub hint: &'a str,    // Additional explanation for the user.
-    pub warning: String,  // The value is determined automatically.
-    pub error: String,    // The value is determined automatically.
+    pub required: bool,  // Mandatory field.
+    pub unique: bool,    // The unique value of a field in a collection.
+    pub disabled: bool,  // Blocks access and modification of the element.
+    pub readonly: bool,  // Specifies that the field cannot be modified by the user.
+    pub is_hide: bool,   // Hide field from user.
+    pub other_attrs: String, // Example: r# "autofocus tabindex="some number" size="some numberString::new()#.
+    pub css_classes: String, // Example: "class-name-1 class-name-2".
+    pub hint: String,        // Additional explanation for the user.
+    pub warning: String,     // The value is determined automatically.
+    pub error: String,       // The value is determined automatically.
     pub alert: String, // Alert message for the entire web form. The value is determined automatically.
 }
 
-impl<'a> Default for InputColor<'a> {
+impl Default for InputColor {
     fn default() -> Self {
         Self {
-            id: "",
-            label: "",
-            widget: "InputColor",
-            input_type: "color",
-            name: "",
+            id: String::new(),
+            label: String::new(),
+            widget: String::from("InputColor"),
+            input_type: String::from("color"),
+            name: String::new(),
             value: None,
-            placeholder: "",
-            pattern: "",
+            placeholder: String::new(),
+            pattern: String::new(),
             minlength: 0_usize,
             maxlength: 256_usize,
             required: false,
@@ -46,9 +46,9 @@ impl<'a> Default for InputColor<'a> {
             disabled: false,
             readonly: false,
             is_hide: false,
-            other_attrs: "",
-            css_classes: "",
-            hint: "",
+            other_attrs: String::new(),
+            css_classes: String::new(),
+            hint: String::new(),
             warning: String::new(),
             error: String::new(),
             alert: String::new(),
@@ -56,7 +56,7 @@ impl<'a> Default for InputColor<'a> {
     }
 }
 
-impl<'a> InputColor<'a> {
+impl InputColor {
     pub fn set(&mut self, value: &str) {
         self.value = Some(String::from(value));
     }
