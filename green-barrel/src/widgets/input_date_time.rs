9315937e6bce702f0cfa54a61524fdb109d5a477
kebasyaty <kebasyaty@gmail.com>
2022-08-10 09:23:51 +0300

    Fix widgets

--- green-barrel/src/widgets/input_date_time.rs
+++ green-barrel/src/widgets/input_date_time.rs
@@ -4,51 +4,51 @@ use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-pub struct InputDateTime<'a> {
-    pub id: &'a str, // The value is determined automatically. Format: "model-name--field-name".
-    pub label: &'a str, // Web form field name.
-    pub widget: &'a str, // Widget name.
-    pub input_type: &'a str, // The value is determined automatically.
-    pub name: &'a str, // The value is determined automatically.
+pub struct InputDateTime {
+    pub id: String, // The value is determined automatically. Format: "model-name--field-name".
+    pub label: String, // Web form field name.
+    pub widget: String, // Widget name.
+    pub input_type: String, // The value is determined automatically.
+    pub name: String, // The value is determined automatically.
     pub value: Option<String>, // Default value.
-    pub placeholder: &'a str, // Displays prompt text.
-    pub pattern: &'a str, // Validating a field using a client-side regex (Only for text, search, tel, url, email, and password controls).
-    pub required: bool,   // Mandatory field.
-    pub unique: bool,     // The unique value of a field in a collection.
-    pub disabled: bool,   // Blocks access and modification of the element.
-    pub readonly: bool,   // Specifies that the field cannot be modified by the user.
-    pub min: &'a str,     // The lower value for entering a number or date.
-    pub max: &'a str,     // The top value for entering a number or date.
-    pub is_hide: bool,    // Hide field from user.
-    pub other_attrs: &'a str, // Example: r# "autofocus tabindex="some number" size="some number""#.
-    pub css_classes: &'a str, // Example: "class-name-1 class-name-2".
-    pub hint: &'a str,    // Additional explanation for the user.
-    pub warning: String,  // The value is determined automatically.
-    pub error: String,    // The value is determined automatically.
+    pub placeholder: String, // Displays prompt text.
+    pub pattern: String, // Validating a field using a client-side regex (Only for text, search, tel, url, email, and password controls).
+    pub required: bool,  // Mandatory field.
+    pub unique: bool,    // The unique value of a field in a collection.
+    pub disabled: bool,  // Blocks access and modification of the element.
+    pub readonly: bool,  // Specifies that the field cannot be modified by the user.
+    pub min: String,     // The lower value for entering a number or date.
+    pub max: String,     // The top value for entering a number or date.
+    pub is_hide: bool,   // Hide field from user.
+    pub other_attrs: String, // Example: r# "autofocus tabindex="some number" size="some numberString::new()#.
+    pub css_classes: String, // Example: "class-name-1 class-name-2".
+    pub hint: String,        // Additional explanation for the user.
+    pub warning: String,     // The value is determined automatically.
+    pub error: String,       // The value is determined automatically.
     pub alert: String, // Alert message for the entire web form. The value is determined automatically.
 }
 
-impl<'a> Default for InputDateTime<'a> {
+impl Default for InputDateTime {
     fn default() -> Self {
         Self {
-            id: "",
-            label: "",
-            widget: "InputDateTime",
-            input_type: "text",
-            name: "",
+            id: String::new(),
+            label: String::new(),
+            widget: String::from("InputDateTime"),
+            input_type: String::from("text"),
+            name: String::new(),
             value: None,
-            placeholder: "",
-            pattern: "",
+            placeholder: String::new(),
+            pattern: String::new(),
             required: false,
             unique: false,
             disabled: false,
             readonly: false,
-            min: "",
-            max: "",
+            min: String::new(),
+            max: String::new(),
             is_hide: false,
-            other_attrs: "",
-            css_classes: "",
-            hint: "",
+            other_attrs: String::new(),
+            css_classes: String::new(),
+            hint: String::new(),
             warning: String::new(),
             error: String::new(),
             alert: String::new(),
@@ -56,7 +56,7 @@ impl<'a> Default for InputDateTime<'a> {
     }
 }
 
-impl<'a> InputDateTime<'a> {
+impl InputDateTime {
     pub fn set(&mut self, value: &str) {
         self.value = Some(String::from(value));
     }
--- green-barrel/src/widgets/input_file.rs
+++ green-barrel/src/widgets/input_file.rs
@@ -4,45 +4,45 @@ use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-pub struct InputFile<'a> {
-    pub id: &'a str, // The value is determined automatically. Format: "model-name--field-name".
-    pub label: &'a str, // Web form field name.
-    pub widget: &'a str, // Widget name.
-    pub input_type: &'a str, // The value is determined automatically.
-    pub name: &'a str, // The value is determined automatically.
+pub struct InputFile {
+    pub id: String, // The value is determined automatically. Format: "model-name--field-name".
+    pub label: String, // Web form field name.
+    pub widget: String, // Widget name.
+    pub input_type: String, // The value is determined automatically.
+    pub name: String, // The value is determined automatically.
     pub value: Option<String>, // Default value.
-    pub accept: &'a str, // Example: "image/jpeg,image/png,image/gif"
-    pub placeholder: &'a str, // Displays prompt text.
+    pub accept: String, // Example: "image/jpeg,image/png,image/gif"
+    pub placeholder: String, // Displays prompt text.
     pub required: bool, // Mandatory field.
     pub disabled: bool, // Blocks access and modification of the element.
     pub readonly: bool, // Specifies that the field cannot be modified by the user.
     pub is_hide: bool, // Hide field from user.
-    pub other_attrs: &'a str, // Example: r# "autofocus tabindex="some number" size="some number""#.
-    pub css_classes: &'a str, // Example: "class-name-1 class-name-2".
-    pub hint: &'a str, // Additional explanation for the user.
-    pub warning: String, // The value is determined automatically.
-    pub error: String, // The value is determined automatically.
+    pub other_attrs: String, // Example: r# "autofocus tabindex="some number" size="some numberString::new()#.
+    pub css_classes: String, // Example: "class-name-1 class-name-2".
+    pub hint: String,        // Additional explanation for the user.
+    pub warning: String,     // The value is determined automatically.
+    pub error: String,       // The value is determined automatically.
     pub alert: String, // Alert message for the entire web form. The value is determined automatically.
 }
 
-impl<'a> Default for InputFile<'a> {
+impl Default for InputFile {
     fn default() -> Self {
         Self {
-            id: "",
-            label: "",
-            widget: "InputFile",
-            input_type: "file",
-            name: "",
+            id: String::new(),
+            label: String::new(),
+            widget: String::from("InputFile"),
+            input_type: String::from("file"),
+            name: String::new(),
             value: None,
-            accept: "",
-            placeholder: "",
+            accept: String::new(),
+            placeholder: String::new(),
             required: false,
             disabled: false,
             readonly: false,
             is_hide: false,
-            other_attrs: "",
-            css_classes: "",
-            hint: "",
+            other_attrs: String::new(),
+            css_classes: String::new(),
+            hint: String::new(),
             warning: String::new(),
             error: String::new(),
             alert: String::new(),
@@ -50,7 +50,7 @@ impl<'a> Default for InputFile<'a> {
     }
 }
 
-impl<'a> InputFile<'a> {
+impl InputFile {
     pub fn set(&mut self, value: &str) {
         self.value = Some(String::from(value));
     }
