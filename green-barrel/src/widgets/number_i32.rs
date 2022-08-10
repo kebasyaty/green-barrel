ba63a019190590ba05a53624c9949a5e6262382c
kebasyaty <kebasyaty@gmail.com>
2022-08-10 09:29:34 +0300

    Fix widgets

--- green-barrel/src/widgets/number_i32.rs
+++ green-barrel/src/widgets/number_i32.rs
@@ -4,40 +4,40 @@ use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-pub struct NumberI32<'a> {
-    pub id: &'a str, // The value is determined automatically. Format: "model-name--field-name".
-    pub label: &'a str, // Web form field name.
-    pub widget: &'a str, // Widget name.
-    pub input_type: &'a str, // The value is determined automatically.
-    pub name: &'a str, // The value is determined automatically.
+pub struct NumberI32 {
+    pub id: String, // The value is determined automatically. Format: "model-name--field-name".
+    pub label: String, // Web form field name.
+    pub widget: String, // Widget name.
+    pub input_type: String, // The value is determined automatically.
+    pub name: String, // The value is determined automatically.
     pub value: Option<i32>, // Default value.
-    pub placeholder: &'a str, // Displays prompt text.
+    pub placeholder: String, // Displays prompt text.
     pub required: bool, // Mandatory field.
     pub unique: bool, // The unique value of a field in a collection.
     pub disabled: bool, // Blocks access and modification of the element.
     pub readonly: bool, // Specifies that the field cannot be modified by the user.
-    pub step: i32,   // Increment step for numeric fields.
+    pub step: i32,  // Increment step for numeric fields.
     pub min: Option<i32>, // The lower value for entering a number or date.
     pub max: Option<i32>, // The top value for entering a number or date.
     pub is_hide: bool, // Hide field from user.
-    pub other_attrs: &'a str, // Example: r# "autofocus tabindex="some number" size="some number""#.
-    pub css_classes: &'a str, // Example: "class-name-1 class-name-2".
-    pub hint: &'a str, // Additional explanation for the user.
-    pub warning: String, // The value is determined automatically.
-    pub error: String, // The value is determined automatically.
+    pub other_attrs: String, // Example: r# "autofocus tabindex="some number" size="some numberString::new()#.
+    pub css_classes: String, // Example: "class-name-1 class-name-2".
+    pub hint: String,        // Additional explanation for the user.
+    pub warning: String,     // The value is determined automatically.
+    pub error: String,       // The value is determined automatically.
     pub alert: String, // Alert message for the entire web form. The value is determined automatically.
 }
 
-impl<'a> Default for NumberI32<'a> {
+impl Default for NumberI32 {
     fn default() -> Self {
         Self {
-            id: "",
-            label: "",
-            widget: "NumberI32",
-            input_type: "number",
-            name: "",
+            id: String::new(),
+            label: String::new(),
+            widget: String::from("NumberI32"),
+            input_type: String::from("number"),
+            name: String::new(),
             value: None,
-            placeholder: "",
+            placeholder: String::new(),
             required: false,
             unique: false,
             disabled: false,
@@ -46,9 +46,9 @@ impl<'a> Default for NumberI32<'a> {
             min: None,
             max: None,
             is_hide: false,
-            other_attrs: "",
-            css_classes: "",
-            hint: "",
+            other_attrs: String::new(),
+            css_classes: String::new(),
+            hint: String::new(),
             warning: String::new(),
             error: String::new(),
             alert: String::new(),
@@ -56,7 +56,7 @@ impl<'a> Default for NumberI32<'a> {
     }
 }
 
-impl<'a> NumberI32<'a> {
+impl NumberI32 {
     pub fn set(&mut self, value: i32) {
         self.value = Some(value);
     }
