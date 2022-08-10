0a7f73e080d71504ec2ef1768c68d84d1984f004
kebasyaty <kebasyaty@gmail.com>
2022-08-10 12:13:09 +0300

    Add SelectF64Mult widget

--- green-barrel/src/widgets.rs
+++ green-barrel/src/widgets.rs
@@ -30,6 +30,7 @@ pub mod range_i64;
 pub mod range_u32;
 pub mod select_f64;
 pub mod select_f64_dyn;
+pub mod select_f64_mult;
 pub mod select_i32;
 pub mod select_i32_dyn;
 pub mod select_i32_mult;
