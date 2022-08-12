8f07155571eefcb3a4aa065cede6175980972f9f
kebasyaty <kebasyaty@gmail.com>
2022-08-12 16:55:21 +0300

    Update widgets module

--- green-barrel/src/widgets.rs
+++ green-barrel/src/widgets.rs
@@ -1,13 +1,14 @@
 //! For control of fields on the server and client side.
 
+// bool
 pub mod check_box;
+// hidden
 pub mod hidden_date_time;
 pub mod hidden_hash;
+// text
 pub mod input_color;
 pub mod input_date;
 pub mod input_date_time;
-pub mod input_email;
-pub mod input_file;
 pub mod input_image;
 pub mod input_ip;
 pub mod input_ipv4;
@@ -17,6 +18,11 @@ pub mod input_phone;
 pub mod input_slug;
 pub mod input_text;
 pub mod input_url;
+pub mod text_area;
+// file
+pub mod input_email;
+pub mod input_file;
+// number
 pub mod number_f64;
 pub mod number_i32;
 pub mod number_i64;
@@ -30,6 +36,7 @@ pub mod range_f64;
 pub mod range_i32;
 pub mod range_i64;
 pub mod range_u32;
+// select
 pub mod select_f64;
 pub mod select_f64_dyn;
 pub mod select_f64_mult;
@@ -50,4 +57,3 @@ pub mod select_u32;
 pub mod select_u32_dyn;
 pub mod select_u32_mult;
 pub mod select_u32_mult_dyn;
-pub mod text_area;
