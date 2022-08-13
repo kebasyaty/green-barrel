63662a55cd8e8161c127498914cc0fc70360f75b
kebasyaty <kebasyaty@gmail.com>
2022-08-13 08:45:10 +0300

    Add doc comment

--- green-barrel/src/models/creator.rs
+++ green-barrel/src/models/creator.rs
@@ -1,5 +1,31 @@
 //! For describe a model with user defaults.
 
+/// For describe a model with user defaults.
+///
+/// # Example:
+///
+/// ```
+/// #[Model]
+/// #[derive(Serialize, Deserialize, Default, Debug)]
+/// pub struct ModelName {
+///     title: InputText
+/// }
+///
+/// impl Creator for ModelName {
+///     fn custom_default() -> Self {
+///         Self {
+///             title: InputText {
+///                 label: String::from("Title"),
+///                 required: true,
+///                 unique: true,
+///                 ..Default::default()
+///             },
+///             ..Default::default()
+///         }
+///     }
+/// }
+/// ```
+///
 pub trait Creator {
     fn custom_default() -> Self;
 }
