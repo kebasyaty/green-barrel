902262cadea9aa6349cefc600724cceddcfe75d7
kebasyaty <kebasyaty@gmail.com>
2022-08-14 15:46:22 +0300

    Update check()

--- green-barrel/src/models/db_query_api/paladins.rs
+++ green-barrel/src/models/db_query_api/paladins.rs
@@ -383,25 +383,30 @@ pub trait QPaladins: Main + Caching + Hooks + Validation + AdditionalValidation 
 
                     // Validation of `unique`.
                     // -----------------------------------------------------------------------------
-                    if widget_name != "InputPassword" && final_widget.unique {
-                        Self::check_unique(hash, field_name, &bson_field_value, &coll)
-                            .unwrap_or_else(|err| {
-                                is_err_symptom = true;
-                                if !widget_type.contains("hidden") && !final_widget.is_hide {
-                                    final_widget.error =
-                                        Self::accumula_err(&final_widget, &err.to_string())
-                                            .unwrap();
-                                } else {
-                                    Err(format!(
-                                        "\n\nModel: `{}` > Field: `{}` ; \
-                                        Method: `check()` => {}\n\n",
-                                        model_name,
-                                        field_name,
-                                        err.to_string()
-                                    ))
-                                    .unwrap()
-                                }
-                            });
+                    let unique = final_widget.get("unique");
+                    if unique.is_some() {
+                        let is_unique = unique.unwrap().as_bool().unwrap();
+                        if widget_name != "InputPassword" && is_unique {
+                            Self::check_unique(hash, field_name, &field_value_bson, &coll)
+                                .unwrap_or_else(|err| {
+                                    is_err_symptom = true;
+                                    if !is_hide {
+                                        *final_widget.get_mut("error").unwrap() = json!(
+                                            Self::accumula_err(&final_widget, &err.to_string())
+                                                .unwrap()
+                                        );
+                                    } else {
+                                        Err(format!(
+                                            "\n\nModel: `{}` > Field: `{}` ; \
+                                                Method: `check()` => {}\n\n",
+                                            model_name,
+                                            field_name,
+                                            err.to_string()
+                                        ))
+                                        .unwrap()
+                                    }
+                                });
+                        }
                     }
 
                     // Validation in regular expression (email, password, etc...).
