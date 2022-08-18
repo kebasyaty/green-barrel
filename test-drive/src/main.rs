e5b233e62ba9300951307da7509925a9fca4eb88
kebasyaty <kebasyaty@gmail.com>
2022-08-18 15:56:33 +0300

    Update test-drive

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -37,30 +37,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Convert Model
     // *********************************************************************************************
-    //println!("{:?}", models::UserProfile::to_wig()?);
     //println!("{}", models::UserProfile::to_json()?);
     //println!("{}", models::UserProfile::model_to_json_for_admin()?);
-    /*
-    println!(
-        "Html code:\n{}",
-        models::UserProfile::to_html(
-            Some("/login"),
-            Some(HttpMethod::POST),
-            Some(Enctype::Multipart)
-        )?
-    );
-    */
     //
-    /*
-       println!(
-           "Widget map:\n{:?}",
-           models::UserProfile::find_one_to_wig(doc! {"username": "user_34"}, None)?
-       );
-    */
     /*
         println!(
             "Model instance:\n{:?}",
-            models::UserProfile::find_one_to_model_instance(doc! {"username": "user_38"}, None)?
+            models::UserProfile::find_one_to_model_instance(doc! {"username": "user_1"}, None)?
         );
     */
 
@@ -81,29 +64,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Boolean: {}", output_data.is_valid());
     println!("Hash: {}", output_data.hash());
     println!("Object Id: {:?}", output_data.object_id());
-    println!("Created at: {}", user.get_created_at());
-    println!("Updated at: {}", user.get_updated_at());
+    //
     // Printing errors to the console ( for development ).
     if !output_data.is_valid() {
         output_data.print_err();
     }
     //
-    //println!("Slug: {}\n\n", output_data.to_wig().get("slug").unwrap().value);
-    //
-    //println!("bson::Document:\n{:?}\n\n", output_data.get_doc());
-    //println!("Widget map:\n{:?}\n\n", output_data.to_wig());
     //println!("Json:\n{}\n\n", output_data.to_json()?);
     //println!("Json for admin:\n{}\n\n", output_data.to_json_for_admin()?);
-    /*
-    println!(
-        "Html:\n{}\n\n",
-        output_data.to_html(
-            Some("/login"),
-            Some(HttpMethod::POST),
-            Some(Enctype::Multipart)
-        )?
-    );
-    */
     */
 
     // Create document in database.
@@ -114,6 +82,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Object Id: {:?}", output_data.object_id());
     println!("Created at: {}", user.get_created_at());
     println!("Updated at: {}", user.get_updated_at());
+    //
     // Printing errors to the console ( for development ).
     if !output_data.is_valid() {
         output_data.print_err();
@@ -121,23 +90,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //
     println!(
         "Slug: {}\n\n",
-        output_data.to_wig().get("slug").unwrap().value
+        output_data
+            .to_json()
+            .get("slug")
+            .unwrap()
+            .get("value")
+            .unwrap()
+            .as_str()
+            .unwrap()
     );
     //
-    //println!("bson::Document:\n{:?}\n\n", output_data.get_doc());
-    //println!("Widget map:\n{:?}\n\n", output_data.to_wig());
     //println!("Json:\n{}\n\n", output_data.to_json()?);
     //println!("Json for admin:\n{}\n\n", output_data.to_json_for_admin()?);
-    /*
-    println!(
-        "Html:\n{}\n\n",
-        output_data.to_html(
-            Some("/login"),
-            Some(HttpMethod::POST),
-            Some(Enctype::Multipart)
-        )?
-    );
-    */
 
     // Update document in database.
     // ---------------------------------------------------------------------------------------------
@@ -158,20 +122,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             output_data.to_wig().get("slug").unwrap().value
         );
         //
-        //println!("bson::Document:\n{:?}\n\n", output_data.get_doc());
-        //println!("Widget map:\n{:?}\n\n", output_data.to_wig());
         //println!("Json:\n{}\n\n", output_data.to_json()?);
         //println!("Json for admin:\n{}\n\n", output_data.to_json_for_admin()?);
-        /*
-        println!(
-            "Html:\n{}\n\n",
-            output_data.to_html(
-                Some("/login"),
-                Some(HttpMethod::POST),
-                Some(Enctype::Multipart)
-            )?
-        );
-        */
     }
     //
     Ok(())
