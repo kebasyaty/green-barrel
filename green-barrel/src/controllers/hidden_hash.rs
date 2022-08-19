ac9a4b6bf6033831c4b1fe7087fab9b8ace3162b
kebasyaty <kebasyaty@gmail.com>
2022-08-19 16:11:11 +0300

    Fix HiddenHash

--- green-barrel/src/controllers/hidden_hash.rs
+++ green-barrel/src/controllers/hidden_hash.rs
@@ -11,7 +11,6 @@ pub struct HiddenHash {
     pub input_type: String, // "hidden | text" - The value is determined automatically.
     pub name: String, // The value is determined automatically.
     pub value: Option<String>, // Default value.
-    pub default: Option<String>, // Value by default
     pub placeholder: String, // Displays prompt text.
     pub pattern: String, // Validating a field using a client-side regex (Only for text, search, tel, url, email, and password controls).
     pub minlength: usize, // The minimum number of characters allowed in the text.
@@ -38,7 +37,6 @@ impl Default for HiddenHash {
             input_type: String::from("hidden"), // "hidden | text"
             name: String::new(),
             value: None,
-            default: None,
             placeholder: String::new(),
             pattern: String::new(),
             minlength: 12,
