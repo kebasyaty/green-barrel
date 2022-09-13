3cb4787ef569ddeb6c443424758e0d7a83bf176f
kebasyaty <kebasyaty@gmail.com>
2022-09-13 08:26:34 +0300

    Add thumbnails sorting and validation

--- metamorphose/src/lib.rs
+++ metamorphose/src/lib.rs
@@ -365,6 +365,20 @@ fn impl_create_model(args: &Vec<NestedMeta>, ast: &mut DeriveInput) -> TokenStre
                             field_name
                         ))?
                     }
+                    // Thumbnails sorting and validation.
+                    if let Some(arr) = instance_json_val.get(field_name).unwrap().get("thumbnails") {
+                        let mut arr = serde_json::from_value::<Vec<(String, u32)>>(arr)?;
+                        arr.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+                        let valid_size_names: [&str; 4] = ["xs", "sm", "md", "lg"];
+                        for size in arr.iter() {
+                            if !valid_size_names.contains(&size.0.as_str()) {
+                                Err(format!(
+                                    "\n\nField: `{}` => Valid size names - `xs`, `sm`, `md`, `lg`.\n\n",
+                                    field_name
+                                ))?
+                            }
+                        }
+                    }
                     // Add `id` and `name`
                     *instance_json_val
                         .get_mut(field_name)
