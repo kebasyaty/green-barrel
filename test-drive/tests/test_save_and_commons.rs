03cf1dc49a2ba43465a4aced9ed6084508f0f899
kebasyaty <kebasyaty@gmail.com>
2022-09-27 08:51:38 +0300

    Fix test_save_and_commons

--- test-drive/tests/test_save_and_commons.rs
+++ test-drive/tests/test_save_and_commons.rs
@@ -199,102 +199,70 @@ fn test_save_and_commons() -> Result<(), Box<dyn Error>> {
     // Testing
     // =============================================================================================
     type TestModel = data_test::TestModel;
-    //
-    // No data
-    // ---------------------------------------------------------------------------------------------
-    let mut test_model = TestModel::new()?;
-    let output_data = test_model.save(None, None)?;
-    assert!(
-        output_data.is_valid(),
-        "is_valid(): {}",
-        output_data.err_msg()
-    );
-    assert!(!output_data.hash().is_empty(), "hash() == is_empty()");
-    assert!(
-        output_data.created_at().is_some(),
-        "created_at() != is_some()"
-    );
-    assert!(
-        output_data.updated_at().is_some(),
-        "updated_at() != is_some()"
-    );
-    assert!(output_data.obj_id()?.is_some(), "obj_id() != is_some()");
-    assert!(!output_data.json()?.is_empty(), "json() == is_empty()");
-    assert!(
-        !output_data.json_for_admin()?.is_empty(),
-        "json_for_admin() == is_empty()"
-    );
-    test_model = output_data.update()?;
-    assert!(
-        test_model.slug.get().is_none(),
-        "test_model.slug.get() != is_none()"
-    );
-
-    // With data
-    // ---------------------------------------------------------------------------------------------
-    let mut test_model = TestModel::new()?;
-    test_model.checkbox.set(true);
-    test_model.date.set("1900-01-31");
-    test_model.datetime.set("1900-01-31T00:00");
-    test_model.file.set(FileData {
-        path: "./media/hello_world.odt".into(),
-        url: "/media/hello_world.odt".into(),
-        ..Default::default()
-    });
-    test_model.image.set(ImageData {
-        path: "./media/no-image-found.png".into(),
-        url: "/media/no-image-found.png".into(),
-        ..Default::default()
-    });
-    test_model.number_i32.set(0);
-    test_model.radio_i32.set(0);
-    test_model.range_i32.set(0);
-    test_model.number_u32.set(0);
-    test_model.radio_u32.set(0);
-    test_model.range_u32.set(0);
-    test_model.number_i64.set(0);
-    test_model.radio_i64.set(0);
-    test_model.range_i64.set(0);
-    test_model.number_f64.set(0.0);
-    test_model.radio_f64.set(0.0);
-    test_model.range_f64.set(0.0);
-    test_model.radio_text.set("Some text");
-    test_model.select_text.set("Some text");
-    test_model.select_text_dyn.set("Some text");
-    test_model
-        .select_text_mult
-        .set(vec!["Some text", "Some text 2"]);
-    test_model
-        .select_text_mult_dyn
-        .set(vec!["Some text", "Some text 2"]);
-    test_model.select_i32.set(0);
-    test_model.select_i32_dyn.set(0);
-    test_model.select_i32_mult.set(vec![0, 1]);
-    test_model.select_i32_mult_dyn.set(vec![0, 1]);
-    test_model.select_u32.set(0);
-    test_model.select_u32_dyn.set(0);
-    test_model.select_u32_mult.set(vec![0, 1]);
-    test_model.select_u32_mult_dyn.set(vec![0, 1]);
-    test_model.select_i64.set(0);
-    test_model.select_i64_dyn.set(0);
-    test_model.select_i64_mult.set(vec![0, 1]);
-    test_model.select_i64_mult_dyn.set(vec![0, 1]);
-    test_model.select_f64.set(0.0);
-    test_model.select_f64_dyn.set(0.0);
-    test_model.select_f64_mult.set(vec![0.0, 1.0]);
-    test_model.select_f64_mult_dyn.set(vec![0.0, 1.0]);
-    test_model.text.set("Some text");
-    test_model.color.set("#ffffff");
-    test_model.email.set("jane32@enhanceronly.com");
-    test_model.password.set("j2972K4R3uQeVFPF");
-    test_model.phone.set("+1 202-918-2132");
-    test_model.url.set("https://ru.wikipedia.org/wiki/URL");
-    test_model.ip.set("192.168.123.132");
-    test_model.ipv4.set("192.168.50.1");
-    test_model.ipv6.set("1050:0:0:0:5:600:300c:326b");
-    test_model.textarea.set("Some text");
 
     for _ in 1..=10 {
+        let mut test_model = TestModel::new()?;
+        test_model.checkbox.set(true);
+        test_model.date.set("1900-01-31");
+        test_model.datetime.set("1900-01-31T00:00");
+        test_model.file.set(FileData {
+            path: "./media/hello_world.odt".into(),
+            url: "/media/hello_world.odt".into(),
+            ..Default::default()
+        });
+        test_model.image.set(ImageData {
+            path: "./media/no-image-found.png".into(),
+            url: "/media/no-image-found.png".into(),
+            ..Default::default()
+        });
+        test_model.number_i32.set(0);
+        test_model.radio_i32.set(0);
+        test_model.range_i32.set(0);
+        test_model.number_u32.set(0);
+        test_model.radio_u32.set(0);
+        test_model.range_u32.set(0);
+        test_model.number_i64.set(0);
+        test_model.radio_i64.set(0);
+        test_model.range_i64.set(0);
+        test_model.number_f64.set(0.0);
+        test_model.radio_f64.set(0.0);
+        test_model.range_f64.set(0.0);
+        test_model.radio_text.set("Some text");
+        test_model.select_text.set("Some text");
+        test_model.select_text_dyn.set("Some text");
+        test_model
+            .select_text_mult
+            .set(vec!["Some text", "Some text 2"]);
+        test_model
+            .select_text_mult_dyn
+            .set(vec!["Some text", "Some text 2"]);
+        test_model.select_i32.set(0);
+        test_model.select_i32_dyn.set(0);
+        test_model.select_i32_mult.set(vec![0, 1]);
+        test_model.select_i32_mult_dyn.set(vec![0, 1]);
+        test_model.select_u32.set(0);
+        test_model.select_u32_dyn.set(0);
+        test_model.select_u32_mult.set(vec![0, 1]);
+        test_model.select_u32_mult_dyn.set(vec![0, 1]);
+        test_model.select_i64.set(0);
+        test_model.select_i64_dyn.set(0);
+        test_model.select_i64_mult.set(vec![0, 1]);
+        test_model.select_i64_mult_dyn.set(vec![0, 1]);
+        test_model.select_f64.set(0.0);
+        test_model.select_f64_dyn.set(0.0);
+        test_model.select_f64_mult.set(vec![0.0, 1.0]);
+        test_model.select_f64_mult_dyn.set(vec![0.0, 1.0]);
+        test_model.text.set("Some text");
+        test_model.color.set("#ffffff");
+        test_model.email.set("jane32@enhanceronly.com");
+        test_model.password.set("j2972K4R3uQeVFPF");
+        test_model.phone.set("+1 202-918-2132");
+        test_model.url.set("https://ru.wikipedia.org/wiki/URL");
+        test_model.ip.set("192.168.123.132");
+        test_model.ipv4.set("192.168.50.1");
+        test_model.ipv6.set("1050:0:0:0:5:600:300c:326b");
+        test_model.textarea.set("Some text");
+
         let output_data = test_model.save(None, None)?;
 
         assert!(
@@ -323,6 +291,10 @@ fn test_save_and_commons() -> Result<(), Box<dyn Error>> {
             "test_model.slug.get() != is_none()"
         );
     }
+
+    // count_documents
+    let result = TestModel::count_documents(None, None)?;
+    assert_eq!(result, 10, "count_documents() != 10");
 
     // Delete test database
     // =============================================================================================
