ad54cf119fbbba3e69198f3606728b0c9ce1ea79
kebasyaty <kebasyaty@gmail.com>
2022-10-25 12:02:08 +0300

    Fix check()

--- green-barrel/src/models/db_query_api/paladins.rs
+++ green-barrel/src/models/db_query_api/paladins.rs
@@ -1587,10 +1587,13 @@ pub trait QPaladins: Main + Caching + Hooks + Validation + AdditionalValidation 
                 if field_type == "InputFile" {
                     let value = field.get("value").unwrap();
                     let default = field.get("default").unwrap();
-                    if !value.is_null() && !default.is_null() {
+                    if !value.is_null() {
                         let file_data = serde_json::from_value::<FileData>(value.clone())?;
-                        let file_data_default =
-                            serde_json::from_value::<FileData>(default.clone())?;
+                        let file_data_default = if !default.is_null() {
+                            serde_json::from_value::<FileData>(default.clone())?
+                        } else {
+                            FileData::default()
+                        };
                         // Exclude files by default.
                         if file_data.path != file_data_default.path {
                             let path = Path::new(&file_data.path);
@@ -1608,10 +1611,13 @@ pub trait QPaladins: Main + Caching + Hooks + Validation + AdditionalValidation 
                 } else if field_type == "InputImage" {
                     let value = field.get("value").unwrap();
                     let default = field.get("default").unwrap();
-                    if !value.is_null() && !default.is_null() {
+                    if !value.is_null() {
                         let img_data = serde_json::from_value::<ImageData>(value.clone())?;
-                        let img_data_default =
-                            serde_json::from_value::<ImageData>(default.clone())?;
+                        let img_data_default = if !default.is_null() {
+                            serde_json::from_value::<ImageData>(default.clone())?
+                        } else {
+                            ImageData::default()
+                        };
                         // Exclude files by default.
                         if img_data.path != img_data_default.path {
                             let path = Path::new(&img_data.path);
