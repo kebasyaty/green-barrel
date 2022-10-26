2e06a122ee862b37ce2e0b590e629726c994c7ae
kebasyaty <kebasyaty@gmail.com>
2022-10-26 09:47:45 +0300

    Update check()

--- green-barrel/src/models/db_query_api/paladins.rs
+++ green-barrel/src/models/db_query_api/paladins.rs
@@ -959,7 +959,23 @@ pub trait QPaladins: Main + Caching + Hooks + Validation + AdditionalValidation 
                         let media_url = final_field.get("media_url").unwrap().as_str().unwrap();
                         let target_dir = final_field.get("target_dir").unwrap().as_str().unwrap();
                         let date_slug = slugify(chrono::Utc::now().to_rfc3339()[..10].to_string());
-                        fs::create_dir_all(format!("{media_root}/{target_dir}/{date_slug}/{}"))?;
+                        fs::create_dir_all(format!("{media_root}/{target_dir}/{date_slug}"))?;
+                        let extension = {
+                            let path = Path::new(file_data.path.as_str());
+                            path.extension().unwrap().to_str().unwrap()
+                        };
+                        let mut new_file_name;
+                        let mut new_file_path;
+                        loop {
+                            new_file_name = format!("{}.{extension}", Uuid::new_v4());
+                            new_file_path = Path::new(media_root)
+                                .join(target_dir)
+                                .join(date_slug.as_str())
+                                .join(new_file_name.as_str());
+                            if !new_file_path.as_path().exists() {
+                                break;
+                            }
+                        }
                     }
                     //
                     // Get file metadata.
