584a4b8ce20ce8583dd536ab85f05a7e5328678f
kebasyaty <kebasyaty@gmail.com>
2022-10-27 10:48:37 +0300

    Fix check()

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -50,8 +50,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Create model instance.
     // *********************************************************************************************
     let mut user = models::User::new()?;
-    user.username.set("user_1");
-    user.email.set("user_1_@noreply.net");
+    user.username.set("user_2");
+    user.email.set("user_2_@noreply.net");
     user.password.set("12345678");
     user.confirm_password.value = Some("12345678".to_string()); // Example without the set() method
     user.is_staff.set(true);
@@ -114,7 +114,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         // Update instance.
         user = output_data.update()?;
 
-        user.username.set("user_1_update");
+        user.username.set("user_2_update");
 
         let output_data = user.save(None, None)?;
         if output_data.is_valid() {
