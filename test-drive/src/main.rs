b338e9c3731b2212286a7c7a5175642384b98ff8
kebasyaty <kebasyaty@gmail.com>
2022-10-28 11:22:52 +0300

    Update test-drive

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -50,8 +50,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Create model instance.
     // *********************************************************************************************
     let mut user = models::User::new()?;
-    user.username.set("user_11");
-    user.email.set("user_11_@noreply.net");
+    user.username.set("user_12");
+    user.email.set("user_12_@noreply.net");
     user.password.set("12345678");
     user.confirm_password.value = Some("12345678".to_string()); // Example without the set() method
     user.is_staff.set(true);
@@ -114,7 +114,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         // Update instance.
         user = output_data.update()?;
 
-        user.username.set("new_user_11");
+        user.username.set("new_user_12");
         let output_data = user.save(None, None)?;
 
         if output_data.is_valid() {
