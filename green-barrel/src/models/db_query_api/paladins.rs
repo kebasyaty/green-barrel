3721301f05474962d283f30df2b1f8e02c30cbb5
kebasyaty <kebasyaty@gmail.com>
2022-10-28 12:31:58 +0300

    Fix check()

--- green-barrel/src/models/db_query_api/paladins.rs
+++ green-barrel/src/models/db_query_api/paladins.rs
@@ -872,8 +872,10 @@ pub trait QPaladins: Main + Caching + Hooks + Validation + AdditionalValidation 
                             let val = final_field.get("default").unwrap();
                             if !val.is_null() {
                                 file_default = serde_json::from_value::<FileData>(val.clone())?;
-                                const_value = val.clone();
-                                is_use_default = true;
+                                if image_data.path.is_empty() {
+                                    const_value = val.clone();
+                                    is_use_default = true;
+                                }
                             } else {
                                 file_default = FileData::default();
                             }
@@ -1009,8 +1011,10 @@ pub trait QPaladins: Main + Caching + Hooks + Validation + AdditionalValidation 
                             let val = final_field.get("default").unwrap();
                             if !val.is_null() {
                                 image_default = serde_json::from_value::<ImageData>(val.clone())?;
-                                const_value = val.clone();
-                                is_use_default = true;
+                                if image_data.path.is_empty() {
+                                    const_value = val.clone();
+                                    is_use_default = true;
+                                }
                             } else {
                                 image_default = ImageData::default();
                             }
