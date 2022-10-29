931d56a9c59ed40436086d91bea6ffd153bd636f
kebasyaty <kebasyaty@gmail.com>
2022-10-29 12:32:52 +0300

    Fix .gitignore

--- test-drive/tests/test_save_and_commons.rs
+++ test-drive/tests/test_save_and_commons.rs
@@ -152,16 +152,8 @@ fn test_save_and_commons() -> Result<(), Box<dyn Error>> {
         test_model.checkbox.set(true);
         test_model.date.set("1900-01-31");
         test_model.datetime.set("1900-01-31T00:00");
-        test_model.file.set(FileData {
-            path: "./media/default/no_file.odt".into(),
-            url: "/media/default/no_file.odt".into(),
-            ..Default::default()
-        });
-        test_model.image.set(ImageData {
-            path: "./media/default/no_image.png".into(),
-            url: "/media/default/no_image.png".into(),
-            ..Default::default()
-        });
+        test_model.file.set(FileData::default());
+        test_model.image.set(ImageData::default());
         test_model.number_i32.set(0);
         test_model.radio_i32.set(0);
         test_model.range_i32.set(0);
