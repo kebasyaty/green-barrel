b8a560d85903f6c84fac391501211d3d62748d0d
kebasyaty <kebasyaty@gmail.com>
2022-11-09 10:32:22 +0200

    Update run_fixture()

--- green-barrel/src/models/fixtures.rs
+++ green-barrel/src/models/fixtures.rs
@@ -1,7 +1,10 @@
 //! To populate the database with pre-created data.
 
+use serde::{de::DeserializeOwned, ser::Serialize};
 use std::error::Error;
 use std::path::Path;
+
+use crate::models::caching::Caching;
 
 /// To populate the database with pre-created data.
 /// fixture_name - Name of the fixture file in the ./fixtures directory, no extension (.json).
@@ -12,16 +15,22 @@ use std::path::Path;
 /// ```
 /// ```
 ///
-pub trait Fixtures {
-    fn run_fixture(fixture_name: &str, _unique_field: &str) -> Result<(), Box<dyn Error>> {
+pub trait Fixtures: Caching {
+    fn run_fixture(fixture_name: &str, _unique_field: &str) -> Result<(), Box<dyn Error>>
+    where
+        Self: Serialize + DeserializeOwned + Sized,
+    {
+        // Get meta-data
+        let meta = Self::meta()?;
         // Create path
         let file_path = format!("./fixtures/{fixture_name}.json");
         let fixture_path = Path::new(&file_path);
         // Validation of file.
         if !fixture_path.is_file() {
             Err(format!(
-                "Model: `{model_name} ; Method: \
-                    run_fixture()` => File is missing - {file_path}"
+                "Model: `{} ; Method: \
+                    run_fixture()` => File is missing - {file_path}",
+                meta.model_name
             ))?
         }
 
