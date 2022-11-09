cfc77733f4e5fcd32ac919358c7a812ac0693856
kebasyaty <kebasyaty@gmail.com>
2022-11-09 10:24:28 +0200

    Update run_fixture()

--- green-barrel/src/models/fixtures.rs
+++ green-barrel/src/models/fixtures.rs
@@ -1,6 +1,7 @@
 //! To populate the database with pre-created data.
 
 use std::error::Error;
+use std::path::Path;
 
 /// To populate the database with pre-created data.
 /// fixture_name - Name of the fixture file in the ./fixtures directory, no extension (.json).
@@ -12,8 +13,18 @@ use std::error::Error;
 /// ```
 ///
 pub trait Fixtures {
-    fn run_fixture(_fixture_name: &str, _unique_field: &str) -> Result<(), Box<dyn Error>> {
-        //
+    fn run_fixture(fixture_name: &str, _unique_field: &str) -> Result<(), Box<dyn Error>> {
+        // Create path
+        let file_path = format!("./fixtures/{fixture_name}.json");
+        let fixture_path = Path::new(&file_path);
+        // Validation of file.
+        if !fixture_path.is_file() {
+            Err(format!(
+                "Model: `{model_name} ; Method: \
+                    run_fixture()` => File is missing - {file_path}"
+            ))?
+        }
+
         Ok(())
     }
 }
