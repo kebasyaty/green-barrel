eccc4ada8c43e9632acf3af06f422719ee104ce2
kebasyaty <kebasyaty@gmail.com>
2022-11-09 10:07:58 +0200

    Update comments

--- green-barrel/src/models/fixtures.rs
+++ green-barrel/src/models/fixtures.rs
@@ -5,6 +5,12 @@ use std::error::Error;
 /// To populate the database with pre-created data.
 /// fixture_name - Name of the fixture file in the ./fixtures directory, no extension (.json).
 /// unique_field - The name of any unique field in the Model.
+///
+/// # Example:
+///
+/// ```
+/// ```
+///
 pub trait Fixtures {
     fn run_fixture(_fixture_name: &str, _unique_field: &str) -> Result<(), Box<dyn Error>> {
         //
