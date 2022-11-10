99443564e742be24bca13742ab6f589d14b4c2ef
kebasyaty <kebasyaty@gmail.com>
2022-11-10 09:34:45 +0200

    Update run_fixture()

--- green-barrel/src/models/fixtures.rs
+++ green-barrel/src/models/fixtures.rs
@@ -1,8 +1,8 @@
 //! To populate the database with pre-created data.
 
 use serde::{de::DeserializeOwned, ser::Serialize};
-use std::error::Error;
-use std::path::Path;
+use serde_json::Value;
+use std::{error::Error, fs, io::ErrorKind};
 
 use crate::models::caching::Caching;
 
@@ -43,14 +43,42 @@ pub trait Fixtures: Caching {
     {
         // Get meta-data
         let meta = Self::meta()?;
-        // Create path
-        let file_path = format!("./fixtures/{fixture_name}.json");
-        let fixture_path = Path::new(&file_path);
-        // Validation of file.
-        if !fixture_path.is_file() {
+        let fields_name = &meta.fields_name;
+        // Get fixtures list
+        let json_val = {
+            // Create path
+            let fixture_path = format!("./fixtures/{fixture_name}.json");
+            // Get json-line
+            let json_str = fs::read_to_string(fixture_path.clone()).unwrap_or_else(|error| {
+                if error.kind() == ErrorKind::NotFound {
+                    Err(format!(
+                        "Model: `{} ; Method: \
+                    run_fixture()` => File is missing - {fixture_path}",
+                        meta.model_name
+                    ))
+                    .unwrap()
+                } else {
+                    Err(format!(
+                        "Model: `{} ; Method: \
+                    run_fixture()` => Problem opening the file: {:?}",
+                        meta.model_name, error
+                    ))
+                    .unwrap()
+                }
+            });
+            serde_json::from_str::<Value>(json_str.as_str())?
+        };
+        //
+        if let Some(fixtures_vec) = json_val.as_array() {
+            for fixture in fixtures_vec {
+                for field_name in fields_name {
+                    //
+                }
+            }
+        } else {
             Err(format!(
                 "Model: `{} ; Method: \
-                    run_fixture()` => File is missing - {file_path}",
+                    run_fixture()` => Fixture does not contain an array of objects.",
                 meta.model_name
             ))?
         }
