d767ecf15c3302012401b9bad62ad5749f7785c2
kebasyaty <kebasyaty@gmail.com>
2022-11-11 19:17:02 +0200

    Fix tests

--- test-drive/tests/test_save_and_commons.rs
+++ test-drive/tests/test_save_and_commons.rs
@@ -167,30 +167,6 @@ fn test_save_and_commons() -> Result<(), Box<dyn Error>> {
         test_model.radio_f64.set(0.0);
         test_model.range_f64.set(0.0);
         test_model.radio_text.set("Some text");
-        test_model.select_text.set("Some text");
-        test_model.select_text_dyn.set("Some text");
-        test_model
-            .select_text_mult
-            .set(vec!["Some text", "Some text 2"]);
-        test_model
-            .select_text_mult_dyn
-            .set(vec!["Some text", "Some text 2"]);
-        test_model.select_i32.set(0);
-        test_model.select_i32_dyn.set(0);
-        test_model.select_i32_mult.set(vec![0, 1]);
-        test_model.select_i32_mult_dyn.set(vec![0, 1]);
-        test_model.select_u32.set(0);
-        test_model.select_u32_dyn.set(0);
-        test_model.select_u32_mult.set(vec![0, 1]);
-        test_model.select_u32_mult_dyn.set(vec![0, 1]);
-        test_model.select_i64.set(0);
-        test_model.select_i64_dyn.set(0);
-        test_model.select_i64_mult.set(vec![0, 1]);
-        test_model.select_i64_mult_dyn.set(vec![0, 1]);
-        test_model.select_f64.set(0.0);
-        test_model.select_f64_dyn.set(0.0);
-        test_model.select_f64_mult.set(vec![0.0, 1.0]);
-        test_model.select_f64_mult_dyn.set(vec![0.0, 1.0]);
         test_model.text.set("Some text");
         test_model.color.set("#ffffff");
         test_model.email.value = Some(format!("x{num}@x.xx"));
