cd38e645dbf0b0af289b97430e179525ee9dab74
kebasyaty <kebasyaty@gmail.com>
2022-11-14 17:15:52 +0200

    Fix test

--- test-drive/tests/test_save_and_commons.rs
+++ test-drive/tests/test_save_and_commons.rs
@@ -187,8 +187,8 @@ fn test_save_and_commons() -> Result<(), Box<dyn Error>> {
         //
         test_model = output_data.update()?;
         assert!(
-            test_model.slug.get().is_none(),
-            "test_model.slug.get() != is_none()"
+            test_model.slug.get().is_some(),
+            "test_model.slug.get() != is_some()"
         );
         //
         assert!(!output_data.hash().is_empty(), "hash() == is_empty()");
