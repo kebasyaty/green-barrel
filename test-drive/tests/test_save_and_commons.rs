6a83cdb37d0a081d9954bd4871bdc6bbad796faa
kebasyaty <kebasyaty@gmail.com>
2022-12-03 09:00:35 +0200

    add test

---
+++ test-drive/tests/test_save_and_commons.rs
@@ -0,0 +1,366 @@
+use green_barrel::test_tool::del_test_db;
+use green_barrel::*;
+use metamorphose::Model;
+use mongodb::{bson::doc, sync::Client};
+use parking_lot::RwLock;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use std::{collections::HashMap, error::Error, fs, path::Path};
+
+mod settings {
+    pub const PROJECT_NAME: &str = "test_project_name";
+    // The unique key for this test.
+    // To generate a key (This is not an advertisement): https://randompasswordgen.com/
+    // Valid characters: a-z A-Z 0-9
+    // Size: 16
+    pub const UNIQUE_PROJECT_KEY: &str = "SkC1UDJ3Z4ucCMW8";
+    //
+    pub const SERVICE_NAME: &str = "test_service_name";
+    pub const DATABASE_NAME: &str = "test_database_name";
+    pub const DB_QUERY_DOCS_LIMIT: u32 = 1000;
+}
+
+mod models {
+    use super::*;
+    use settings::{
+        DATABASE_NAME, DB_QUERY_DOCS_LIMIT, PROJECT_NAME, SERVICE_NAME, UNIQUE_PROJECT_KEY,
+    };
+
+    #[Model]
+    #[derive(Serialize, Deserialize, Default)]
+    pub struct TestModel {
+        pub checkbox: CheckBox,
+        //
+        pub date: InputDate,
+        pub datetime: InputDateTime,
+        //
+        pub file: InputFile,
+        pub image: InputImage,
+        //
+        pub number_i32: NumberI32,
+        pub radio_i32: RadioI32,
+        pub range_i32: RangeI32,
+        //
+        pub number_u32: NumberU32,
+        pub radio_u32: RadioU32,
+        pub range_u32: RangeU32,
+        //
+        pub number_i64: NumberI64,
+        pub radio_i64: RadioI64,
+        pub range_i64: RangeI64,
+        //
+        pub number_f64: NumberF64,
+        pub radio_f64: RadioF64,
+        pub range_f64: RangeF64,
+        //
+        pub radio_text: RadioText,
+        //
+        pub select_text: SelectText,
+        pub select_text_dyn: SelectTextDyn,
+        pub select_text_mult: SelectTextMult,
+        pub select_text_mult_dyn: SelectTextMultDyn,
+        //
+        pub select_i32: SelectI32,
+        pub select_i32_dyn: SelectI32Dyn,
+        pub select_i32_mult: SelectI32Mult,
+        pub select_i32_mult_dyn: SelectI32MultDyn,
+        //
+        pub select_u32: SelectU32,
+        pub select_u32_dyn: SelectU32Dyn,
+        pub select_u32_mult: SelectI32Mult,
+        pub select_u32_mult_dyn: SelectU32MultDyn,
+        //
+        pub select_i64: SelectI64,
+        pub select_i64_dyn: SelectI64Dyn,
+        pub select_i64_mult: SelectI64Mult,
+        pub select_i64_mult_dyn: SelectI64MultDyn,
+        //
+        pub select_f64: SelectF64,
+        pub select_f64_dyn: SelectF64Dyn,
+        pub select_f64_mult: SelectF64Mult,
+        pub select_f64_mult_dyn: SelectF64MultDyn,
+        //
+        pub text: InputText,
+        pub slug: AutoSlug,
+        pub color: InputColor,
+        pub email: InputEmail,
+        pub password: InputPassword,
+        pub phone: InputPhone,
+        pub url: InputUrl,
+        pub ip: InputIP,
+        pub ipv4: InputIPv4,
+        pub ipv6: InputIPv6,
+        pub textarea: TextArea,
+    }
+
+    impl Control for TestModel {
+        fn custom_default() -> Self {
+            Self {
+                image: InputImage {
+                    required: true,
+                    default: Some(ImageData {
+                        path: "./media/default/no_image.png".into(),
+                        url: "/media/default/no_image.png".into(),
+                        ..Default::default()
+                    }),
+                    thumbnails: vec![
+                        ("xs".into(), 150),
+                        ("sm".into(), 300),
+                        ("md".into(), 600),
+                        ("lg".into(), 1200),
+                    ],
+                    is_quality: false,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }
+        }
+    }
+}
+
+mod migration {
+    use super::*;
+
+    // Get metadata list
+    pub fn get_model_key_list() -> Result<Vec<String>, Box<dyn Error>> {
+        let model_key_list = vec![models::TestModel::key()?];
+        Ok(model_key_list)
+    }
+
+    // Migration
+    pub fn run_migration(
+        meta_store: &Arc<RwLock<HashMap<String, Meta>>>,
+        client: &Client,
+        _validators: &HashMap<String, Regex>,
+        _media_dir: &HashMap<String, String>,
+    ) -> Result<(), Box<dyn Error>> {
+        // Caching metadata.
+        models::TestModel::caching(meta_store, client)?;
+
+        // Remove test databases
+        // ( Test databases may remain in case of errors )
+        del_test_db(
+            settings::PROJECT_NAME,
+            settings::UNIQUE_PROJECT_KEY,
+            get_model_key_list()?,
+            meta_store,
+            client,
+        )?;
+
+        // Monitor initialization.
+        let monitor = Monitor {
+            project_name: settings::PROJECT_NAME,
+            unique_project_key: settings::UNIQUE_PROJECT_KEY,
+            // Register models
+            model_key_list: get_model_key_list()?,
+        };
+        monitor.migrat(meta_store, client)?;
+
+        Ok(())
+    }
+}
+
+mod app_state {
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    pub struct AppState {
+        media_root: String,
+        media_url: String,
+    }
+
+    impl Default for AppState {
+        fn default() -> Self {
+            Self {
+                // Root partition for storing files.
+                media_root: String::from("./media"),
+                // Url address to the root section.
+                media_url: String::from("/media"),
+            }
+        }
+    }
+
+    pub fn get_app_state() -> Result<AppState, Box<dyn Error>> {
+        let path = Path::new("./AppState.toml");
+        if !path.is_file() {
+            fs::File::create(path)?;
+            let cfg = AppState::default();
+            confy::store_path(path, cfg)?;
+        }
+        Ok(confy::load_path::<AppState>(path)?)
+    }
+
+    pub fn get_media_dir(app_state: AppState) -> HashMap<String, String> {
+        [
+            ("media_root".into(), app_state.media_root),
+            ("media_url".into(), app_state.media_url),
+        ]
+        .iter()
+        .cloned()
+        .collect()
+    }
+}
+
+// TEST
+// #################################################################################################
+#[test]
+fn test_save_and_commons() -> Result<(), Box<dyn Error>> {
+    // This is required for all projects.
+    // =============================================================================================
+    let app_state = app_state::get_app_state()?;
+    let media_dir = app_state::get_media_dir(app_state);
+    let meta_store = Arc::new(get_meta_store());
+    let client = Client::with_uri_str("mongodb://localhost:27017/")?;
+    let validators = get_validators()?;
+    migration::run_migration(&meta_store, &client, &validators, &media_dir)?;
+
+    // YOUR CODE ...
+    // =============================================================================================
+    type TestModel = models::TestModel;
+
+    for num in 1..=10 {
+        let target_file = format!("./media/tmp/no_file_2_{num}.odt");
+        let target_image = format!("./media/tmp/no_image_2_{num}.png");
+        fs::copy("./media/default/no_file.odt", target_file.clone())?;
+        fs::copy("./media/default/no_image.png", target_image.clone())?;
+
+        let mut test_model = TestModel::new(&meta_store)?;
+        test_model.checkbox.set(true);
+        test_model.date.set("1900-01-31");
+        test_model.datetime.set("1900-01-31T00:00");
+        test_model.file.set(target_file.as_str());
+        test_model.image.set(target_image.as_str());
+        test_model.number_i32.set(0);
+        test_model.range_i32.set(0);
+        test_model.number_u32.set(0);
+        test_model.range_u32.set(0);
+        test_model.number_i64.set(0);
+        test_model.range_i64.set(0);
+        test_model.number_f64.set(0.0);
+        test_model.range_f64.set(0.0);
+        test_model.text.set("Some text");
+        test_model.color.set("#ffffff");
+        test_model.email.value = Some(format!("x{num}@x.xx"));
+        test_model.password.set("j2972K4R3uQeVFPF");
+        test_model.phone.set("+1 202-918-2132");
+        test_model.url.set("https://ru.wikipedia.org/wiki/URL");
+        test_model.ip.set("192.168.123.132");
+        test_model.ipv4.set("192.168.50.1");
+        test_model.ipv6.set("1050:0:0:0:5:600:300c:326b");
+        test_model.textarea.set("Some text");
+
+        let output_data =
+            test_model.save(&meta_store, &client, &validators, &media_dir, None, None)?;
+        test_model = output_data.update()?;
+
+        assert!(
+            output_data.is_valid(),
+            "is_valid(): {}",
+            output_data.err_msg()
+        );
+        assert!(
+            test_model.slug.get().is_some(),
+            "test_model.slug.get() != is_some()"
+        );
+        assert!(!output_data.hash().is_empty(), "hash() == is_empty()");
+        assert!(
+            output_data.created_at().is_some(),
+            "created_at() != is_some()"
+        );
+        assert!(
+            output_data.updated_at().is_some(),
+            "updated_at() != is_some()"
+        );
+        assert!(output_data.obj_id()?.is_some(), "obj_id() != is_some()");
+        assert!(!output_data.json()?.is_empty(), "json() == is_empty()");
+        assert!(
+            output_data.json_for_admin()?.is_some(),
+            "json_for_admin() != is_some()"
+        );
+    }
+
+    // count_documents
+    let result = TestModel::count_documents(&meta_store, &client, None, None)?;
+    assert_eq!(result, 10, "count_documents() != 10");
+    // estimated_document_count
+    let result = TestModel::estimated_document_count(&meta_store, &client, None)?;
+    assert_eq!(result, 10, "estimated_document_count() != 10");
+    // find_many_to_doc_list
+    let result = TestModel::find_many_to_doc_list(&meta_store, &client, None, None)?;
+    assert!(result.is_some(), "find_many_to_doc_list() != is_some()");
+    assert_eq!(
+        result.unwrap().len(),
+        10,
+        "find_many_to_doc_list(): len() != 10"
+    );
+    // find_many_to_json
+    let result = TestModel::find_many_to_json(&meta_store, &client, None, None)?;
+    assert!(result.is_some(), "find_many_to_json() != is_some()");
+    // find_one_to_doc
+    let filter = doc! {"email": "x10@x.xx"};
+    let result = TestModel::find_one_to_doc(filter, &meta_store, &client, None)?;
+    assert!(result.is_some(), "find_one_to_doc() != is_some()");
+    // find_one_to_json
+    let filter = doc! {"email": "x5@x.xx"};
+    let result = TestModel::find_one_to_json(filter, &meta_store, &client, None)?;
+    assert!(!result.is_empty(), "find_one_to_json() == is_empty()");
+    // find_one_to_instance
+    let filter = doc! {"email": "x1@x.xx"};
+    let result = TestModel::find_one_to_instance(filter, &meta_store, &client, None)?;
+    assert!(result.is_some(), "find_one_to_instance() != is_some()");
+    // collection_name
+    let result = TestModel::collection_name(&meta_store, &client)?;
+    assert!(!result.is_empty(), "collection_name() == is_empty()");
+    // namespace
+    let result = TestModel::namespace(&meta_store, &client)?;
+    assert!(!result.db.is_empty(), "namespace(): db == is_empty()");
+    assert!(!result.coll.is_empty(), "namespace(): coll == is_empty()");
+    // find_one_and_delete
+    let filter = doc! {"email": "x2@x.xx"};
+    let result = TestModel::find_one_and_delete(filter, &meta_store, &client, None)?;
+    assert!(result.is_some(), "find_one_and_delete() != is_some()");
+    // count_documents
+    let result = TestModel::count_documents(&meta_store, &client, None, None)?;
+    assert_eq!(result, 9, "count_documents() != 9");
+    // estimated_document_count
+    let result = TestModel::estimated_document_count(&meta_store, &client, None)?;
+    assert_eq!(result, 9, "estimated_document_count() != 9");
+    // delete_one
+    let query = doc! {"email": "x3@x.xx"};
+    let result = TestModel::delete_one(query, &meta_store, &client, None)?;
+    assert!(result.is_valid(), "is_valid(): {}", result.err_msg());
+    assert!(result.deleted_count()? == 1, "delete_one() != 1");
+    // count_documents
+    let result = TestModel::count_documents(&meta_store, &client, None, None)?;
+    assert_eq!(result, 8, "count_documents() != 8");
+    // estimated_document_count
+    let result = TestModel::estimated_document_count(&meta_store, &client, None)?;
+    assert_eq!(result, 8, "estimated_document_count() != 8");
+    // delete_many
+    let query = doc! {"email": {"$in": ["x4@x.xx", "x6@x.xx"]}};
+    let result = TestModel::delete_many(query, &meta_store, &client, None)?;
+    assert!(result.is_valid(), "is_valid(): {}", result.err_msg());
+    assert!(
+        result.deleted_count()? == 2,
+        "delete_many(): deleted_count() != 2"
+    );
+    // count_documents
+    let result = TestModel::count_documents(&meta_store, &client, None, None)?;
+    assert_eq!(result, 6, "count_documents() != 6");
+    // estimated_document_count
+    let result = TestModel::estimated_document_count(&meta_store, &client, None)?;
+    assert_eq!(result, 6, "estimated_document_count() != 6");
+
+    // Delete test database
+    // =============================================================================================
+    del_test_db(
+        settings::PROJECT_NAME,
+        settings::UNIQUE_PROJECT_KEY,
+        migration::get_model_key_list()?,
+        &meta_store,
+        &client,
+    )?;
+
+    Ok(())
+}
