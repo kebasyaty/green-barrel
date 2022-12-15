04f123974c206aac8e8d13c5aa2c69baf1869836
kebasyaty <kebasyaty@gmail.com>
2022-12-15 19:36:27 +0200

    add example - files

---
+++ green-barrel/examples/files/src/main.rs
@@ -0,0 +1,47 @@
+mod migration;
+mod models;
+mod settings;
+
+use chrono::Local;
+use green_barrel::*;
+use mongodb::{bson::doc, Client};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+    let client = Client::with_uri_str(uri).await?;
+    //
+    migration::run_migration(&client).await?;
+
+    // Specify the time zone (optional).
+    // ( For convert to Utc )
+    let tz = Some(Local::now().format("%z").to_string());
+
+    // Create user
+    // *********************************************************************************************
+    let mut user = models::User::new().await?;
+    user.username.set("user_1");
+    user.avatar.set("./some_files/avatar.png", None);
+    user.resume.set("./some_files/resume.odt", None);
+
+    // Add document in database
+    // *********************************************************************************************
+    println!("\n\nCreate document in database:\n");
+    let output_data = user.save(&client, &tz, None, None).await?;
+    //user = output_data.update()?;
+
+    if output_data.is_valid() {
+        let filter = doc! {"username": "user_1"};
+        if let Some(user_doc) = models::User::find_one_to_doc(filter, &client, None).await? {
+            println!("{:#?}", user_doc);
+        } else {
+            panic!("The document is missing!");
+        }
+    } else {
+        // Printing errors to the console ( for development ).
+        output_data.print_err();
+    }
+
+    Ok(())
+}
---
+++ green-barrel/examples/files/src/models.rs
@@ -0,0 +1,50 @@
+use green_barrel::*;
+use metamorphose::Model;
+use serde::{Deserialize, Serialize};
+
+use crate::settings::{
+    accounts::SERVICE_NAME, APP_NAME, DATABASE_NAME, DB_QUERY_DOCS_LIMIT, UNIQUE_APP_KEY,
+};
+
+#[Model()]
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct User {
+    pub username: InputText,
+    pub avatar: InputImage,
+    pub resume: InputFile,
+}
+
+impl Control for User {
+    fn custom_default() -> Self {
+        Self {
+            username: InputText {
+                unique: true,
+                required: true,
+                maxlength: 150,
+                ..Default::default()
+            },
+            avatar: InputImage {
+                label: "Avatar".into(),
+                default: Some(ImageData {
+                    path: "./resources/media/default/no_image.png".into(),
+                    url: "/media/default/no_image.png".into(),
+                    ..Default::default()
+                }),
+                target_dir: "users/avatars".into(),
+                thumbnails: vec![("xs".into(), 150), ("sm".into(), 300)], // all sizes: "xs","sm","md","lg"
+                ..Default::default()
+            },
+            resume: InputFile {
+                default: Some(FileData {
+                    path: "./resources/media/default/no_file.odt".into(),
+                    url: "/media/default/no_file.odt".into(),
+                    ..Default::default()
+                }),
+                label: "Resume".into(),
+                target_dir: "users/resume".into(),
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+    }
+}
