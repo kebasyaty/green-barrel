47eb820fabee8f8cf2e84c43f9c37905efa43a3a
kebasyaty <kebasyaty@gmail.com>
2022-12-15 14:42:23 +0200

    Update examples

---
+++ green-barrel/examples/hooks/src/models.rs
@@ -0,0 +1,55 @@
+use async_trait::async_trait;
+use green_barrel::*;
+use metamorphose::Model;
+use mongodb::Client;
+use serde::{Deserialize, Serialize};
+
+use crate::settings::{
+    accounts::SERVICE_NAME, APP_NAME, DATABASE_NAME, DB_QUERY_DOCS_LIMIT, UNIQUE_APP_KEY,
+};
+
+#[Model(is_use_hooks = true)]
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct User {
+    pub username: InputText,
+    pub slug: AutoSlug,
+    pub first_name: InputText,
+    pub last_name: InputText,
+    pub email: InputEmail,
+    pub phone: InputPhone,
+}
+
+impl Control for User {
+    fn custom_default() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+}
+
+#[async_trait(?Send)]
+impl Hooks for User {
+    async fn pre_create(&self, _client: &Client) {
+        println!("!!!Pre Create!!!");
+    }
+    //
+    async fn post_create(&self, _client: &Client) {
+        println!("!!!Post Create!!!");
+    }
+    //
+    async fn pre_update(&self, _client: &Client) {
+        println!("!!!Pre Update!!!");
+    }
+    //
+    async fn post_update(&self, _client: &Client) {
+        println!("!!!Post Update!!!");
+    }
+    //
+    async fn pre_delete(&self, _client: &Client) {
+        println!("!!!Pre Delet!!!");
+    }
+    //
+    async fn post_delete(&self, _client: &Client) {
+        println!("!!!Post Delet!!!");
+    }
+}
