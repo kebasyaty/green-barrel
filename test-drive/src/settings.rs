a9cdc789c05d45ffa9773a6f733940463cf26a2f
kebasyaty <kebasyaty@gmail.com>
2022-12-21 09:09:57 +0200

    Update README.md

--- test-drive/src/settings.rs
+++ test-drive/src/settings.rs
@@ -18,7 +18,7 @@ pub const DATABASE_NAME: &str = "app_name";
 // Size: 16
 // Example: "7rzgacfqQB3B7q7T"
 // To generate a key (This is not an advertisement): https://randompasswordgen.com/
-pub const UNIQUE_APP_KEY: &str = "9yFK09gQ007qzhr9";
+pub const UNIQUE_APP_KEY: &str = "9yFK09gQ008qzhr9";
 //
 pub const DB_QUERY_DOCS_LIMIT: u32 = 1000;
 
