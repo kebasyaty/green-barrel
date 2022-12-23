f7af1f344821e80efd0d8627ab500b8452de0605
kebasyaty <kebasyaty@gmail.com>
2022-12-23 08:44:54 +0200

    Update README.md

--- green-barrel/src/lib.rs
+++ green-barrel/src/lib.rs
@@ -1,5 +1,24 @@
 //! # Green Barrel
-//! ORM-like API MongoDB for Rust.
+//! ### ORM-like API MongoDB for Rust.
+//!
+//! ## Usage:
+//!
+//! [Basic Example](https://github.com/kebasyaty/green-barrel/tree/master/examples/basic "Basic Example")
+//!
+//! ## Model parameters
+//!
+//! **_( all parameters are optional )_**
+//!
+//! | Parameter:          | Default:     | Description:                                                                                         |
+//! | :------------------ | :----------- | :--------------------------------------------------------------------------------------------------- |
+//! | db_query_docs_limit | 1000         | limiting query results.                                                                              |
+//! | is_add_doc          | true         | Create documents in the database. **false** - Alternatively, use it to validate data from web forms. |
+//! | is_up_doc           | true         | Update documents in the database.                                                                    |
+//! | is_del_doc          | true         | Delete documents from the database.                                                                  |
+//! | ignore_fields       | empty string | Fields that are not included in the database (separated by commas).                                  |
+//! | is_use_add_valid    | false        | Allows additional validation - **impl AdditionalValidation for ModelName**.                          |
+//! | is_use_hooks        | false        | Allows hooks methods - **impl Hooks for ModelName**.                                                 |
+//!
 
 pub mod fields;
 pub mod meta_store;
