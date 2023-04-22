5040be7d1b7d0c73dd906b0c6938523989163e14
kebasyaty <kebasyaty@gmail.com>
2023-04-22 15:36:24 +0300

    update example

--- examples/dynamic_fields/src/models.rs
+++ examples/dynamic_fields/src/models.rs
@@ -9,23 +9,23 @@ use crate::settings::{
 #[Model()]
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct User {
-    pub username: InputText,
-    //pub select_text_dyn: SelectTextDyn,
-    pub select_text_mult_dyn: SelectTextMultDyn,
-    //pub select_i32_dyn: SelectI32Dyn,
-    //pub select_i32_mult_dyn: SelectI32MultDyn,
-    //pub select_u32_dyn: SelectU32Dyn,
-    //pub select_u32_mult_dyn: SelectU32MultDyn,
-    //pub select_i64_dyn: SelectI64Dyn,
-    //pub select_i64_mult_dyn: SelectI64MultDyn,
-    //pub select_f64_dyn: SelectF64Dyn,
-    //pub select_f64_mult_dyn: SelectF64MultDyn,
+    pub username: Text,
+    //pub select_text_dyn: ChoiceTextDyn,
+    pub select_text_mult_dyn: ChoiceTextMultDyn,
+    //pub select_i32_dyn: ChoiceI32Dyn,
+    //pub select_i32_mult_dyn: ChoiceI32MultDyn,
+    //pub select_u32_dyn: ChoiceU32Dyn,
+    //pub select_u32_mult_dyn: ChoiceU32MultDyn,
+    //pub select_i64_dyn: ChoiceI64Dyn,
+    //pub select_i64_mult_dyn: ChoiceI64MultDyn,
+    //pub select_f64_dyn: ChoiceF64Dyn,
+    //pub select_f64_mult_dyn: ChoiceF64MultDyn,
 }
 
 impl Control for User {
     fn custom_default() -> Self {
         Self {
-            username: InputText {
+            username: Text {
                 maxlength: 150,
                 required: true,
                 unique: true,
