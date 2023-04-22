bb265c6ceaaebf90011933120076f1ce7c3a61e2
kebasyaty <kebasyaty@gmail.com>
2023-04-22 16:25:13 +0300

    update example

--- examples/fixtures/src/settings.rs
+++ examples/fixtures/src/settings.rs
@@ -18,7 +18,7 @@ pub const DATABASE_NAME: &str = "app_name";
 // Size: 16
 // Example: "7rzgacfqQB3B7q7T"
 // To generate a key (This is not an advertisement): https://randompasswordgen.com/
-pub const UNIQUE_APP_KEY: &str = "RH5bb86N4mg3I0vZ";
+pub const UNIQUE_APP_KEY: &str = "RH5bb86N4mg3I0vY";
 //
 pub const DB_QUERY_DOCS_LIMIT: u32 = 1000;
 
