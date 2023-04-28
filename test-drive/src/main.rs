0971da6bbd7d91b50a141950c6cf17f68bf9d1b4
kebasyaty <kebasyaty@gmail.com>
2023-04-28 19:59:33 +0300

    add comments

--- test-drive/src/main.rs
+++ test-drive/src/main.rs
@@ -5,7 +5,7 @@ use green_barrel::*;
 use mongodb::Client;
 use std::error::Error;
 
-rust_i18n::i18n!("../locales");
+rust_i18n::i18n!("locales");
 
 // Migration
 async fn run_migration(client: &Client) -> Result<(), Box<dyn Error>> {
@@ -26,9 +26,10 @@ async fn run_migration(client: &Client) -> Result<(), Box<dyn Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    //
-    rust_i18n::set_locale("ru"); // defaule = en-us
-                                 //
+    // Change globally current locale
+    // defaule = en-us
+    rust_i18n::set_locale("ru");
+    // Init Client
     let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
     let client = Client::with_uri_str(uri).await?;
     //
