de8df5ed76b1c4345ba3b23e283014df77c8e48b
Gennady Kostyunin <kebasyaty@gmail.com>
2023-08-16 09:02:30 +0300

    Update comments

--- green-barrel/src/fields/bool.rs
+++ green-barrel/src/fields/bool.rs
@@ -1,28 +1,44 @@
 //! Boolean field.
-//! The values are defined in the **checked** parameter.
 
 use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BoolField {
-    pub id: String, // The value is determined automatically. Format: "model-name--field-name".
-    pub label: String, // Web form field name.
-    pub field_type: String, // Field type.
-    pub input_type: String, // The value is determined automatically.
-    pub name: String, // The value is determined automatically.
-    pub value: Option<bool>, // Sets the value of an element.
-    pub default: Option<bool>, // Value by default
-    pub placeholder: String, // Displays prompt text.
-    pub disabled: bool, // Blocks access and modification of the element.
-    pub readonly: bool, // Specifies that the field cannot be modified by the user.
-    pub is_hide: bool, // Hide field from user.
-    pub other_attrs: String, // Example: r# "autofocus tabindex="some number" size="some number""#.
-    pub css_classes: String, // Example: "class-name-1 class-name-2".
-    pub hint: String, // Additional explanation for the user.
-    pub warning: String, // Warning information.
-    pub errors: Vec<String>, // The value is determined automatically.
-    pub group: u32, // To optimize field traversal in the `paladins/check()` method. Hint: It is recommended not to change.
+    /// The value is determined automatically. Format: "model-name--field-name".
+    pub id: String,
+    /// Web form field name.
+    pub label: String,
+    /// Field type.
+    pub field_type: String,
+    /// The value is determined automatically.
+    pub input_type: String,
+    /// The value is determined automatically.
+    pub name: String,
+    /// Sets the value of an element.
+    pub value: Option<bool>,
+    /// Value by default
+    pub default: Option<bool>,
+    /// Displays prompt text.
+    pub placeholder: String,
+    /// Blocks access and modification of the element.
+    pub disabled: bool,
+    /// Specifies that the field cannot be modified by the user.
+    pub readonly: bool,
+    /// Hide field from user.
+    pub is_hide: bool,
+    /// Example: r# "autofocus tabindex="some number" size="some number""#.
+    pub other_attrs: String,
+    /// Example: "class-name-1 class-name-2".
+    pub css_classes: String,
+    /// Additional explanation for the user.
+    pub hint: String,
+    /// Warning information.
+    pub warning: String,
+    /// The value is determined automatically.
+    pub errors: Vec<String>,
+    /// To optimize field traversal in the `paladins/check()` method. Hint: It is recommended not to change.
+    pub group: u32,
 }
 
 impl Default for BoolField {
@@ -50,9 +66,11 @@ impl Default for BoolField {
 }
 
 impl BoolField {
+    // Getter
     pub fn get(&self) -> Option<bool> {
         self.value
     }
+    // Setter
     pub fn set(&mut self, value: bool) {
         self.value = Some(value);
     }
